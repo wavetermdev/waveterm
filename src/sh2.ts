051110cdc1839686f5d41829d8cf2874da02d1b1
sawka <mike.sawka@gmail.com>
2022-10-10 12:08:29 -0700

    fix font loading

--- src/sh2.ts
+++ src/sh2.ts
@@ -11,13 +11,29 @@ import {v4 as uuidv4} from "uuid";
 // @ts-ignore
 let VERSION = __SHVERSION__;
 
+let jbmFontNormal = new FontFace("JetBrains Mono", "url('static/fonts/jetbrains-mono-v13-latin-regular.woff2')", {style: "normal", weight: "400"});
+let jbmFont200 = new FontFace("JetBrains Mono", "url('static/fonts/jetbrains-mono-v13-latin-200.woff2')", {style: "normal", weight: "200"});
+let jbmFont700 = new FontFace("JetBrains Mono", "url('static/fonts/jetbrains-mono-v13-latin-700.woff2')", {style: "normal", weight: "700"});
+let loadPromise = document.fonts.add(jbmFontNormal);
+document.fonts.add(jbmFont200);
+document.fonts.add(jbmFont700);
+jbmFontNormal.load();
+jbmFont200.load();
+jbmFont700.load();
+
 document.addEventListener("DOMContentLoaded", () => {
     let reactElem = React.createElement(Main, null, null);
     let elem = document.getElementById("app");
     let root = createRoot(elem);
-    setTimeout(() => {
+    let isFontLoaded = document.fonts.check("12px 'JetBrains Mono'");
+    if (isFontLoaded) {
         root.render(reactElem);
-    }, 500);
+    }
+    else {
+        document.fonts.ready.then(() => {
+            root.render(reactElem);
+        });
+    }
 });
 
 (window as any).mobx = mobx;
