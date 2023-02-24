3b1198d6b775578974d4f1a6b0e87b44748b9c7a
sawka <mike.sawka@gmail.com>
2023-02-23 19:57:31 -0800

    remove production sourcemap

--- webpack.prod.js
+++ webpack.prod.js
@@ -24,7 +24,7 @@ let merged = merge.merge(common, {
         path: path.resolve(__dirname, "dist"),
         filename: "[name].js",
     },
-    devtool: "source-map",
+    devtool: null,
     optimization: {
         minimize: true,
     },
