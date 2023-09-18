aa5c09368e124b669d64da83e7924307594d1741
sawka <mike.sawka@gmail.com>
2023-09-17 20:32:35 -0700

    bump version to v0.3.1

--- pkg/scbase/scbase.go
+++ pkg/scbase/scbase.go
@@ -32,7 +32,7 @@ const PromptLockFile = "prompt.lock"
 const PromptDirName = "prompt"
 const PromptDevDirName = "prompt-dev"
 const PromptAppPathVarName = "PROMPT_APP_PATH"
-const PromptVersion = "v0.3.0"
+const PromptVersion = "v0.3.1"
 const PromptAuthKeyFileName = "prompt.authkey"
 const MShellVersion = "v0.3.0"
 
