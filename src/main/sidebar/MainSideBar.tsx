b65f4f80f7a32ccd44c9ea70378ca7b85f016b14
Amarsh Anand <amarsh.anand@gmail.com>
2023-09-30 12:12:38 -0700

    starting on Tabs

--- src/main/sidebar/MainSideBar.tsx
+++ src/main/sidebar/MainSideBar.tsx
@@ -142,6 +142,33 @@ class MainSideBar extends React.Component<{}, {}> {
         })();
     }
 
+    getSessions() {
+        if (!GlobalModel.sessionListLoaded.get()) return <div className="item">loading ...</div>;
+        let sessionList = [];
+        let activeSessionId = GlobalModel.activeSessionId.get();
+        for (let session of GlobalModel.sessionList) {
+            if (!session.archived.get() || session.sessionId == activeSessionId) {
+                sessionList.push(session);
+            }
+        }
+        return sessionList.map((session, index) => {
+            const isActive = GlobalModel.activeMainView.get() == "session" && activeSessionId == session.sessionId;
+            return (
+                <>
+                    {/** @TODO: Handle archived sessions and talk to Mike about session settings */}
+                    <div
+                        className={`item hoverEffect ${isActive ? "active" : ""}`}
+                        onClick={() => this.handleSessionClick(session.sessionId)}
+                    >
+                        <span className="hotkey">^⌘</span>
+                        <span className="index">{index + 1}</span>
+                        <span>{session.name.get()}</span>
+                    </div>
+                </>
+            );
+        });
+    }
+
     render() {
         let model = GlobalModel;
         let activeSessionId = model.activeSessionId.get();
@@ -171,149 +198,50 @@ class MainSideBar extends React.Component<{}, {}> {
                 <div className="arrow-container hoverEffect" onClick={this.toggleCollapsed}>
                     <LeftChevronIcon />
                 </div>
-                {false && (
-                    <>
-                        {" "}
-                        <p className="menu-label">Sessions</p>
-                        <ul className="menu-list session-menu-list">
-                            <If condition={!model.sessionListLoaded.get()}>
-                                <li className="menu-loading-message">
-                                    <a>...</a>
-                                </li>
-                            </If>
-                            <If condition={model.sessionListLoaded.get()}>
-                                <For each="session" index="idx" of={sessionList}>
-                                    <li key={session.sessionId}>
-                                        <a
-                                            className={cn({
-                                                "is-active":
-                                                    mainView == "session" && activeSessionId == session.sessionId,
-                                            })}
-                                            onClick={() => this.handleSessionClick(session.sessionId)}
-                                        >
-                                            <If condition={!session.archived.get()}>
-                                                <div className="session-num">
-                                                    <span className="hotkey">^⌘</span>
-                                                    {idx + 1}
-                                                </div>
-                                            </If>
-                                            <If condition={session.archived.get()}>
-                                                <div className="session-num">
-                                                    <i title="archived" className="fa-sharp fa-solid fa-box-archive" />
-                                                </div>
-                                            </If>
-                                            <div>{session.name.get()}</div>
-                                            <div className="flex-spacer" />
-                                            <div
-                                                className="session-gear"
-                                                onClick={(e) => this.openSessionSettings(e, session)}
-                                            >
-                                                <i className="fa-sharp fa-solid fa-gear" />
-                                            </div>
-                                        </a>
-                                    </li>
-                                </For>
-                                <li className="new-session">
-                                    <a onClick={() => this.handleNewSession()}>
-                                        <i className="fa-sharp fa-solid fa-plus" /> New Session
-                                    </a>
-                                </li>
-                            </If>
-                        </ul>
-                    </>
-                )}
-                {false && (
-                    <>
-                        <p className="menu-label display-none">Playbooks</p>
-                        <ul className="menu-list display-none">
-                            <li key="default">
-                                <a onClick={this.handlePlaybookClick}>
-                                    <i className="fa-sharp fa-solid fa-file-lines" /> default
-                                </a>
-                            </li>
-                            <li key="prompt-dev">
-                                <a onClick={this.handlePlaybookClick}>
-                                    <i className="fa-sharp fa-solid fa-file-lines" /> prompt-dev
-                                </a>
-                            </li>
-                        </ul>
-                        <div className="spacer"></div>
-                        <If condition={GlobalModel.debugScreen.get() && activeScreen != null}>
-                            <div>
-                                focus={activeScreen.focusType.get()}
-                                <br />
-                                sline={activeScreen.getSelectedLine()}
-                                <br />
-                                termfocus={activeScreen.termLineNumFocus.get()}
-                                <br />
-                            </div>
-                        </If>
-                        <p className="menu-label">
-                            <a onClick={this.handleConnectionsClick}>Connections</a>
-                        </p>
-                        <ul className="menu-list remotes-menu-list">
-                            <For each="remote" of={remotes}>
-                                <li key={remote.remoteid} className={cn("remote-menu-item")}>
-                                    <a
-                                        className={cn({ "is-active": remote.remoteid == activeRemoteId })}
-                                        onClick={() => this.clickRemote(remote)}
-                                    >
-                                        <RemoteStatusLight remote={remote} />
-                                        {this.remoteDisplayName(remote)}
-                                    </a>
-                                </li>
-                            </For>
-                            <li key="add-remote" className="add-remote">
-                                <a onClick={() => this.handleAddRemote()}>
-                                    <i className="fa-sharp fa-solid fa-plus" /> Add Connection
-                                </a>
-                            </li>
-                        </ul>
-                    </>
-                )}
-                <div className="top">
-                    <div className="item hoverEffect">
-                        <AppsIcon className="icon" />
-                        Apps
-                        <span className="hotkey">&#x2318;A</span>
+                <div className="contents">
+                    <div className="top">
+                        <div className="item hoverEffect">
+                            <AppsIcon className="icon" />
+                            Apps
+                            <span className="hotkey">&#x2318;A</span>
+                        </div>
+                        <div className="item hoverEffect" onClick={this.handleHistoryClick}>
+                            <HistoryIcon className="icon" />
+                            History
+                            <span className="hotkey">&#x2318;H</span>
+                        </div>
+                        <div className="item hoverEffect" onClick={this.handleBookmarksClick}>
+                            <FavouritesIcon className="icon" />
+                            Favourites
+                            <span className="hotkey">&#x2318;B</span>
+                        </div>
+                        <div className="item hoverEffect" onClick={this.handleConnectionsClick}>
+                            <ConnectionsIcon className="icon" />
+                            Connections
+                        </div>
                     </div>
-                    <div className="item hoverEffect" onClick={this.handleHistoryClick}>
-                        <HistoryIcon className="icon" />
-                        History
-                        <span className="hotkey">&#x2318;H</span>
-                    </div>
-                    <div className="item hoverEffect" onClick={this.handleBookmarksClick}>
-                        <FavouritesIcon className="icon" />
-                        Favourites
-                        <span className="hotkey">&#x2318;B</span>
-                    </div>
-                    <div className="item hoverEffect">
-                        <ConnectionsIcon className="icon" />
-                        Connections
-                    </div>
-                </div>
-                <div className="separator" />
-                <div className="middle">
+                    <div className="separator" />
                     <div className="item">
                         <WorkspacesIcon className="icon" />
                         Workspaces
-                        <div className="add_workspace hoverEffect" onClick={this.handleAddRemote}>
+                        <div className="add_workspace hoverEffect" onClick={this.handleNewSession}>
                             <AddIcon />
                         </div>
                     </div>
-                </div>
-                <div className="bottom">
-                    <div className="item hoverEffect" onClick={this.handleSettingsClick}>
-                        <SettingsIcon className="icon" />
-                        Settings
-                    </div>
-                    <div className="item hoverEffect" onClick={() => openLink("https://docs.getprompt.dev")}>
-                        <HelpIcon className="icon" />
-                        Documentation
-                    </div>
-                    <div className="item hoverEffect" onClick={() => openLink("https://discord.gg/XfvZ334gwU")}>
-                        <DiscordIcon className="icon" />
-                        Talk to us
+                    <div className="middle">{this.getSessions()}</div>
+                    <div className="bottom">
+                        <div className="item hoverEffect" onClick={this.handleSettingsClick}>
+                            <SettingsIcon className="icon" />
+                            Settings
+                        </div>
+                        <div className="item hoverEffect" onClick={() => openLink("https://docs.getprompt.dev")}>
+                            <HelpIcon className="icon" />
+                            Documentation
+                        </div>
+                        <div className="item hoverEffect" onClick={() => openLink("https://discord.gg/XfvZ334gwU")}>
+                            <DiscordIcon className="icon" />
+                            Talk to us
+                        </div>
                     </div>
                 </div>
             </div>
