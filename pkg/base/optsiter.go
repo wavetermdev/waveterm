afa7ef3d76eb07b2c26a2ac9f350b94f0b681e8b
Red J Adaya <red.adaya@gmail.com>
2023-10-10 09:19:47 +0800

    remove license (#2)

--- pkg/base/optsiter.go
+++ pkg/base/optsiter.go
@@ -1,9 +1,3 @@
-// Copyright 2022 Dashborg Inc
-//
-// This Source Code Form is subject to the terms of the Mozilla Public
-// License, v. 2.0. If a copy of the MPL was not distributed with this
-// file, You can obtain one at https://mozilla.org/MPL/2.0/.
-
 package base
 
 import "strings"
