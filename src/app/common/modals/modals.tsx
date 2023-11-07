d076fc0c7287e350632eafd73cd5c287e65169c9
sawka <mike.sawka@gmail.com>
2023-11-07 01:24:35 -0800

    remove current version check in about modal (not implemented).  text not selectable (except for version)

--- src/app/common/modals/modals.tsx
+++ src/app/common/modals/modals.tsx
@@ -369,6 +369,13 @@ class AboutModal extends React.Component<{}, {}> {
 
     @boundMethod
     getStatus(isUpToDate: boolean): JSX.Element {
+        // TODO no up-to-date status reporting
+        return (
+            <div className="status updated">
+                <div className="text-selectable">Client Version v0.4.0 20231016-110014</div>
+            </div>
+        );
+        
         if (isUpToDate) {
             return (
                 <div className="status updated">
@@ -376,7 +383,7 @@ class AboutModal extends React.Component<{}, {}> {
                         <i className="fa-sharp fa-solid fa-circle-check" />
                         <span>Up to Date</span>
                     </div>
-                    <div>Client Version v0.4.0 20231016-110014</div>
+                    <div className="selectable">Client Version v0.4.0 20231016-110014</div>
                 </div>
             );
         }
@@ -386,7 +393,7 @@ class AboutModal extends React.Component<{}, {}> {
                     <i className="fa-sharp fa-solid fa-triangle-exclamation" />
                     <span>Outdated Version</span>
                 </div>
-                <div>Client Version v0.4.0 20231016-110014</div>
+                <div className="selectable">Client Version v0.4.0 20231016-110014</div>
                 <div>
                     <button onClick={this.updateApp} className="button color-green text-secondary">
                         Update
@@ -400,7 +407,7 @@ class AboutModal extends React.Component<{}, {}> {
         return (
             <div className={cn("modal about-modal wave-modal is-active")}>
                 <div className="modal-background" />
-                <div className="modal-content about-modal-content">
+                <div className="modal-content about-modal-content unselectable">
                     <div className="modal-content-wrapper">
                         <header className="common-header">
                             <div className="modal-title">About</div>
