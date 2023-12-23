019eaecdaaed0703e4b2f3cb77c1e7f72fee9a27
Red J Adaya <red.adaya@gmail.com>
2023-12-24 04:31:11 +0800

    Immediately hide modal when deleting workspace (#187)
    * immediately hide modal when deleting workspace
    
    * only pop modal when purging is successful

--- src/app/common/modals/settings.tsx
+++ src/app/common/modals/settings.tsx
@@ -440,7 +440,7 @@ class SessionSettingsModal extends React.Component<{}, {}> {
                 return;
             }
             let prtn = GlobalCommandRunner.sessionPurge(this.sessionId);
-            commandRtnHandler(prtn, this.errorMessage);
+            commandRtnHandler(prtn, this.errorMessage, () => GlobalModel.modalsModel.popModal());
         });
     }
 
@@ -783,7 +783,10 @@ class ClientSettingsModal extends React.Component<{}, {}> {
                     <div className="settings-field">
                         <div className="settings-label">Check for Updates</div>
                         <div className="settings-input">
-                            <Toggle checked={!cdata.clientopts.noreleasecheck} onChange={this.handleChangeReleaseCheck} />
+                            <Toggle
+                                checked={!cdata.clientopts.noreleasecheck}
+                                onChange={this.handleChangeReleaseCheck}
+                            />
                         </div>
                     </div>
                     <div className="settings-field">
