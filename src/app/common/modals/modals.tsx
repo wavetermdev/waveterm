863be46a5a841adb04cd9ce22e66b0c768d530d0
Evan Simkowitz <esimkowitz@users.noreply.github.com>
2023-12-24 13:41:34 -0500

    Add acknowledgements link to about, remove unnecessary width setting

--- src/app/common/modals/modals.tsx
+++ src/app/common/modals/modals.tsx
@@ -460,6 +460,15 @@ class AboutModal extends React.Component<{}, {}> {
                             <i className="fa-sharp fa-light fa-book-blank"></i>
                             License
                         </a>
+                        <a
+                            className="wave-button wave-button-link color-standard"
+                            href={util.makeExternLink("https://github.com/wavetermdev/waveterm/blob/main/acknowledgements/README.md")}
+                            rel={"noopener"}
+                            target="_blank"
+                        >
+                            <i className="fa-sharp fa-light fa-heart"></i>
+                            Acknowledgements
+                        </a>
                     </div>
                     <div className="about-section text-standard">&copy; 2023 Command Line Inc.</div>
                 </div>
