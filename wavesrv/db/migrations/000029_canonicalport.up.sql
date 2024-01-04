798c2a67253c9d47d420f6fed46dd7d84fb03901
Sylvia Crowe <software@oneirocosm.com>
2024-01-03 22:51:47 -0800

    simplify up migration logic
    The previous up migration was suboptimal because it was accounting for a
    corner case not found in production. That case no longer needs to be
    considered, so the procedure was simplified.

--- wavesrv/db/migrations/000029_canonicalport.up.sql
+++ wavesrv/db/migrations/000029_canonicalport.up.sql
@@ -1,4 +1,3 @@
 UPDATE remote
-SET remotecanonicalname = case when json_extract(sshopts, '$.issudo') then 'sudo@' else '' end ||
-       remoteuser || '@' || remotehost || COALESCE(':' || json_extract(sshopts, '$.sshport'), '')
+SET remotecanonicalname = remotecanonicalname || COALESCE( ":" || json_extract(sshopts, '$.sshport'), "")
 WHERE json_extract(sshopts, '$.sshport') != 22;
