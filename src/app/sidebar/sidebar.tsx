e2cece188b534f6577ba61e27aab7bfd2e20cefc
Evan Simkowitz <esimkowitz@users.noreply.github.com>
2024-01-25 14:34:27 -0800

    merge fix

--- src/app/sidebar/sidebar.tsx
+++ src/app/sidebar/sidebar.tsx
@@ -20,7 +20,6 @@ import { ReactComponent as SettingsIcon } from "../assets/icons/settings.svg";
 import localizedFormat from "dayjs/plugin/localizedFormat";
 import { GlobalModel, GlobalCommandRunner, Session, VERSION } from "../../model/model";
 import { isBlank, openLink } from "../../util/util";
-import { isBlank, openLink } from "../../util/util";
 import * as constants from "../appconst";
 
 import "./sidebar.less";
