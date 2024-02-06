36e2578a03e6d5bbf9898ceb19200c1090b31928
Red Adaya <red.adaya@gmail.com>
2024-02-06 09:26:46 +0800

    remove styles not related to dropdown

--- src/app/common/elements/cmdtext.tsx
+++ src/app/common/elements/cmdtext.tsx
@@ -2,9 +2,6 @@
 // SPDX-License-Identifier: Apache-2.0
 
 import * as React from "react";
-import * as mobx from "mobx";
-
-import "./cmdtext.less";
 
 function renderCmdText(text: string): any {
     return <span>&#x2318;{text}</span>;
