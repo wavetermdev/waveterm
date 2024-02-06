a084ca4801da1af3893b7eea1f6d34472b1e45bd
Red Adaya <red.adaya@gmail.com>
2024-02-06 09:30:10 +0800

    IconButton doesn't need styles because it extends Button

--- src/app/common/elements/iconbutton.tsx
+++ src/app/common/elements/iconbutton.tsx
@@ -2,11 +2,7 @@
 // SPDX-License-Identifier: Apache-2.0
 
 import * as React from "react";
-import * as mobx from "mobx";
 import { Button } from "./button";
-
-import "./iconbutton.less";
-
 class IconButton extends Button {
     render() {
         const { children, theme, variant = "solid", ...rest } = this.props;
