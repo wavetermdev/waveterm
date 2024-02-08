ef7acda1bba8a9995ff73662c7b0e57de5ea7153
Red Adaya <red.adaya@gmail.com>
2024-02-08 09:01:08 +0800

    cleanup linecontainer model

--- src/model/linecontainer.ts
+++ src/model/linecontainer.ts
@@ -1,122 +1,10 @@
 // Copyright 2023, Command Line Inc.
 // SPDX-License-Identifier: Apache-2.0
 
-import type React from "react";
-import * as mobx from "mobx";
-import { sprintf } from "sprintf-js";
-import { v4 as uuidv4 } from "uuid";
-import { boundMethod } from "autobind-decorator";
-import { debounce } from "throttle-debounce";
-import * as mobxReact from "mobx-react";
-import {
-    handleJsonFetchResponse,
-    base64ToString,
-    stringToBase64,
-    base64ToArray,
-    genMergeData,
-    genMergeDataMap,
-    genMergeSimpleData,
-    boundInt,
-    isModKeyPress,
-} from "../util/util";
 import { TermWrap } from "../plugins/terminal/term";
-import { PluginModel } from "../plugins/plugins";
-import {
-    SessionDataType,
-    LineType,
-    RemoteType,
-    HistoryItem,
-    RemoteInstanceType,
-    RemotePtrType,
-    CmdDataType,
-    FeCmdPacketType,
-    TermOptsType,
-    ScreenDataType,
-    ScreenOptsType,
-    PtyDataUpdateType,
-    ModelUpdateType,
-    UpdateMessage,
-    InfoType,
-    UIContextType,
-    HistoryInfoType,
-    HistoryQueryOpts,
-    FeInputPacketType,
-    RemoteInputPacketType,
-    ContextMenuOpts,
-    RendererContext,
-    RendererModel,
-    PtyDataType,
-    BookmarkType,
-    ClientDataType,
-    HistoryViewDataType,
-    AlertMessageType,
-    HistorySearchParams,
-    FocusTypeStrs,
-    ScreenLinesType,
-    HistoryTypeStrs,
-    RendererPluginType,
-    WindowSize,
-    WebShareOpts,
-    TermContextUnion,
-    RemoteEditType,
-    RemoteViewType,
-    CommandRtnType,
-    WebCmd,
-    WebRemote,
-    OpenAICmdInfoChatMessageType,
-    StatusIndicatorLevel,
-} from "../types/types";
-import * as T from "../types/types";
-import { WSControl } from "./ws";
-import {
-    getMonoFontSize,
-    windowWidthToCols,
-    windowHeightToRows,
-    termWidthFromCols,
-    termHeightFromRows,
-} from "../util/textmeasure";
-import dayjs from "dayjs";
-import localizedFormat from "dayjs/plugin/localizedFormat";
-import customParseFormat from "dayjs/plugin/customParseFormat";
-import { getRendererContext, cmdStatusIsRunning } from "../app/line/lineutil";
-import { MagicLayout } from "../app/magiclayout";
-import { modalsRegistry } from "../app/common/modals/registry";
-import * as appconst from "../app/appconst";
-import { checkKeyPressed, adaptFromReactOrNativeKeyEvent, setKeyUtilPlatform } from "../util/keyutil";
+import { LineType, RendererContext, RendererModel, FocusTypeStrs, WindowSize } from "../types/types";
+import { LineContainerStrs } from "../types/types";
 import { Cmd } from "./cmd";
-
-dayjs.extend(customParseFormat);
-dayjs.extend(localizedFormat);
-
-const RemotePtyRows = 8; // also in main.tsx
-const RemotePtyCols = 80;
-const ProdServerEndpoint = "http://127.0.0.1:1619";
-const ProdServerWsEndpoint = "ws://127.0.0.1:1623";
-const DevServerEndpoint = "http://127.0.0.1:8090";
-const DevServerWsEndpoint = "ws://127.0.0.1:8091";
-const DefaultTermFontSize = 12;
-const MinFontSize = 8;
-const MaxFontSize = 24;
-const InputChunkSize = 500;
-const RemoteColors = ["red", "green", "yellow", "blue", "magenta", "cyan", "white", "orange"];
-const TabColors = ["red", "orange", "yellow", "green", "mint", "cyan", "blue", "violet", "pink", "white"];
-const TabIcons = [
-    "sparkle",
-    "fire",
-    "ghost",
-    "cloud",
-    "compass",
-    "crown",
-    "droplet",
-    "graduation-cap",
-    "heart",
-    "file",
-];
-
-// @ts-ignore
-const VERSION = __WAVETERM_VERSION__;
-// @ts-ignore
-const BUILD = __WAVETERM_BUILD__;
 
 type LineContainerModel = {
     loadTerminalRenderer: (elem: Element, line: LineType, cmd: Cmd, width: number) => void;
@@ -136,7 +24,7 @@ type LineContainerModel = {
     getIdealContentSize(): WindowSize;
     isSidebarOpen(): boolean;
     isLineIdInSidebar(lineId: string): boolean;
-    getContainerType(): T.LineContainerStrs;
+    getContainerType(): LineContainerStrs;
 };
 
 export type { LineContainerModel };
