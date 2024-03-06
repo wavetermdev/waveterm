bd08b760805c4955853ff5a0ea0f9e2785f76a9a
Evan Simkowitz <esimkowitz@users.noreply.github.com>
2024-03-06 12:06:22 -0800

    update scripts

--- buildres/publish-from-staging.sh
+++ buildres/publish-from-staging.sh
@@ -1,10 +1,12 @@
 # Takes a release from our staging bucket and publishes it to the public download bucket.
+# Usage: publish-from-staging.sh <version>
+# Example: publish-from-staging.sh 0.1.0
 
 # Takes the version as an argument
 VERSION=$1
 if [ -z "$VERSION" ]; then
     echo "Usage: $0 <version>"
-    exit 1
+    exit
 fi
 
 aws s3 cp s3://waveterm-github-artifacts/staging/$VERSION/ s3://dl.waveterm.dev/releases/ --recursive
