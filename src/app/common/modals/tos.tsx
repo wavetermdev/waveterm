f31666f037f6486c27227d38ee651541ddf0c566
Sylvia Crowe <software@oneirocosm.com>
2024-03-14 16:49:05 -0700

    add link to telemetry page in the welcome screen

--- src/app/common/modals/tos.tsx
+++ src/app/common/modals/tos.tsx
@@ -48,8 +48,9 @@ class TosModal extends React.Component<{}, {}> {
                                 <div className="item-inner">
                                     <div className="item-title">Telemetry</div>
                                     <div className="item-text">
-                                        We only collect minimal <i>anonymous</i> telemetry data to help us understand
-                                        how many people are using Wave.
+                                        We only collect minimal <i>anonymous</i>{" "}
+                                        <a href="https://docs.waveterm.dev/reference/telemetry">telemetry data</a> to
+                                        help us understand how many people are using Wave.
                                     </div>
                                     <div className="item-field" style={{ marginTop: 2 }}>
                                         <Toggle
