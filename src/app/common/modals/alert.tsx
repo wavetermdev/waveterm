c0c53edb847b8a8e87eb87ef992403c1b314e815
Cole Lashley <colel1410@gmail.com>
2024-03-21 23:10:02 -0700

    Modal keybindings (#484)
    * first draft at modal keybindings
    
    * added modal keybindings and inline settings text edit keybindings
    
    * added switch for keybindings in modal footer
    
    * removed logs
    
    * remove another console.log
    
    * fix userinput keybindings -- should be generic:cancel for 2nd one

--- src/app/common/modals/alert.tsx
+++ src/app/common/modals/alert.tsx
@@ -9,6 +9,7 @@ import { Markdown, Modal, Button, Checkbox } from "@/elements";
 import { GlobalModel, GlobalCommandRunner } from "@/models";
 
 import "./alert.less";
+import { ModalKeybindings } from "../elements/modal";
 
 @mobxReact.observer
 class AlertModal extends React.Component<{}, {}> {
@@ -54,6 +55,7 @@ class AlertModal extends React.Component<{}, {}> {
                 </div>
                 <div className="wave-modal-footer">
                     <If condition={isConfirm}>
+                        <ModalKeybindings onOk={this.handleOK} onCancel={this.closeModal}></ModalKeybindings>
                         <Button className="secondary" onClick={this.closeModal}>
                             Cancel
                         </Button>
@@ -62,6 +64,7 @@ class AlertModal extends React.Component<{}, {}> {
                         </Button>
                     </If>
                     <If condition={!isConfirm}>
+                        <ModalKeybindings onOk={this.handleOK} onCancel={null}></ModalKeybindings>
                         <Button autoFocus={true} onClick={this.handleOK}>
                             Ok
                         </Button>
