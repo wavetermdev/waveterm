2fb7f4657e8df05208660b0092b055899feb6104
sawka <mike.sawka@gmail.com>
2024-03-28 16:34:27 -0700

    fix displayname -- now using hash

--- wavesrv/pkg/scpacket/scpacket.go
+++ wavesrv/pkg/scpacket/scpacket.go
@@ -35,10 +35,10 @@ func (rptr *RemotePtrType) GetDisplayName(baseDisplayName string) string {
 		return name
 	}
 	if rptr.Name != "" {
-		name = name + ":" + rptr.Name
+		name = name + "#" + rptr.Name
 	}
 	if rptr.OwnerId != "" {
-		name = "@" + rptr.OwnerId + ":" + name
+		name = "@" + rptr.OwnerId + "#" + name
 	}
 	return name
 }
