314beb4762387311062be930009c1662e8015aa8
Evan Simkowitz <esimkowitz@users.noreply.github.com>
2024-04-04 19:01:39 -0700

    adjust padding for input, remove debug

--- src/models/input.ts
+++ src/models/input.ts
@@ -564,7 +564,6 @@ class InputModel {
     }
 
     setAIChatFocus() {
-        console.log("setting ai chat focus");
         if (this.aiChatTextAreaRef?.current != null) {
             this.aiChatTextAreaRef.current.focus();
         }
