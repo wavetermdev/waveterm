7f1f0f02d7eeb700179dd00518dd9ff71732ebeb
Evan Simkowitz <esimkowitz@users.noreply.github.com>
2024-05-02 15:01:35 -0700

    wrap actions

--- src/app/workspace/cmdinput/suggestionview.tsx
+++ src/app/workspace/cmdinput/suggestionview.tsx
@@ -1,9 +1,10 @@
 import { AuxiliaryCmdView } from "./auxview";
 import { GlobalModel } from "@/models";
-import cn from "classnames";
+import { clsx } from "clsx";
 import React, { useEffect } from "react";
 import { observer } from "mobx-react";
 import { If } from "tsx-control-statements/components";
+import { action } from "mobx";
 
 import "./suggestionview.less";
 import { getAll, getFirst } from "@/autocomplete/runtime/utils";
@@ -12,13 +13,26 @@ export const AutocompleteSuggestionView: React.FC = observer(() => {
     const inputModel = GlobalModel.inputModel;
     const autocompleteModel = GlobalModel.autocompleteModel;
     const selectedSuggestion = autocompleteModel.getPrimarySuggestionIndex();
-    const updateScroll = (index: number) => {
+
+    const updateScroll = action((index: number) => {
         autocompleteModel.setPrimarySuggestionIndex(index);
         const element = document.getElementsByClassName("suggestion-item")[index] as HTMLElement;
         if (element) {
             element.scrollIntoView({ block: "nearest" });
         }
-    };
+    });
+
+    const closeView = action(() => {
+        inputModel.closeAuxView();
+    });
+
+    const setSuggestion = action((idx: number) => {
+        console.log("setSuggestion", idx);
+        autocompleteModel.applySuggestion(idx);
+        autocompleteModel.loadSuggestions();
+        closeView();
+    });
+
     useEffect(() => {
         const keybindManager = GlobalModel.keybindManager;
 
@@ -47,18 +61,8 @@ export const AutocompleteSuggestionView: React.FC = observer(() => {
             GlobalModel.keybindManager.unregisterDomain("autocomplete");
         };
     });
+
     const suggestions: Fig.Suggestion[] = autocompleteModel.getSuggestions();
-
-    const closeView = () => {
-        inputModel.closeAuxView();
-    };
-
-    const setSuggestion = (idx: number) => {
-        console.log("setSuggestion", idx);
-        autocompleteModel.applySuggestion(idx);
-        autocompleteModel.loadSuggestions();
-        closeView();
-    };
 
     return (
         <AuxiliaryCmdView title="Suggestions" className="suggestions-view" onClose={closeView} scrollable={true}>
@@ -69,7 +73,7 @@ export const AutocompleteSuggestionView: React.FC = observer(() => {
                 <option
                     key={getFirst(suggestion.name)}
                     title={suggestion.description}
-                    className={cn("suggestion-item", { "is-selected": selectedSuggestion === idx })}
+                    className={clsx("suggestion-item", { "is-selected": selectedSuggestion === idx })}
                     onClick={() => {
                         setSuggestion(idx);
                     }}
