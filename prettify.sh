1dba3f2612822885b3fec2ae6d1b5ff06b9bf1ed
MrStashley <colel1410@gmail.com>
2024-05-03 18:18:01 -0700

    removed rebase artifacts

--- prettify.sh
+++
@@ -1,4 +0,0 @@
-dirs=$(git diff main --name-only --diff-filter d | grep -e '\.[tj]sx\?$' | xargs)
-echo dirs: $dirs
-node_modules/prettier/bin-prettier.js --write $dirs
-
--- prettifyAndPush.sh
+++
@@ -1,5 +0,0 @@
-dirs=$(git diff main --name-only --diff-filter d | grep -e '\.[tj]sx\?$' | xargs)
-node_modules/prettier/bin-prettier.js --write $dirs
-git add $dirs
-git commit --amend
-git push
