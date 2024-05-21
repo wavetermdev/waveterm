0eeacecc06038f066353fac3afcc660ec8b36b01
sawka <mike.sawka@gmail.com>
2024-05-21 11:46:54 -0700

    fix typescript errors, use wails models

--- frontend/app/workspace/workspace.tsx
+++ frontend/app/workspace/workspace.tsx
@@ -49,7 +49,7 @@ function Widgets() {
 
     async function createBlock(blockDef: BlockDef) {
         const rtOpts = { termsize: { rows: 25, cols: 80 } };
-        const rtnBlock: BlockData = await BlockService.CreateBlock(blockDef, rtOpts);
+        const rtnBlock: BlockData = (await BlockService.CreateBlock(blockDef, rtOpts)) as BlockData;
         const newBlockAtom = jotai.atom(rtnBlock);
         blockDataMap.set(rtnBlock.blockid, newBlockAtom);
         addBlockIdToTab(activeTabId, rtnBlock.blockid);
