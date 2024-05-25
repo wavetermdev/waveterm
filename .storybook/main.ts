9bbc39405e2c0f44dca741113a9b29667e594b69
Evan Simkowitz <esimkowitz@users.noreply.github.com>
2024-05-24 17:44:41 -0700

    Add configs from Faraday repo

---
+++ .storybook/main.ts
@@ -0,0 +1,23 @@
+import type { StorybookConfig } from "@storybook/react-vite";
+
+const config: StorybookConfig = {
+	stories: ["../lib/**/*.mdx", "../lib/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+	addons: [
+		"@storybook/addon-links",
+		"@storybook/addon-essentials",
+		"@chromatic-com/storybook",
+		"@storybook/addon-interactions",
+	],
+	framework: {
+		name: "@storybook/react-vite",
+		options: {},
+	},
+	docs: {
+		autodocs: "tag",
+	},
+	managerHead: (head) => `
+        ${head}
+        <meta name="robots" content="noindex" />
+        `,
+};
+export default config;
