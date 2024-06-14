043788cc1c77059b49de34b3fb9727d284bb2e78
Evan Simkowitz <esimkowitz@users.noreply.github.com>
2024-06-13 19:46:31 -0700

    Fix Storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,4 +1,6 @@
 import type { StorybookConfig } from "@storybook/react-vite";
+import { UserConfig, mergeConfig } from "vite";
+import electronViteConfig from "../electron.vite.config";
 
 const config: StorybookConfig = {
     stories: ["../frontend/**/*.mdx", "../frontend/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
@@ -28,6 +30,10 @@ const config: StorybookConfig = {
 
     typescript: {
         reactDocgen: "react-docgen-typescript",
+    },
+
+    async viteFinal(config) {
+        return mergeConfig(config, electronViteConfig.renderer as UserConfig);
     },
 };
 export default config;
