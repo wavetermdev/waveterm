44f4a02cf7a93db1bb8dfd34c2ca2e61fd3886ea
Evan Simkowitz <esimkowitz@users.noreply.github.com>
2024-08-08 12:49:56 -0700

    download artifacts to script dir

--- scripts/artifacts/download-staged-artifact.sh
+++ scripts/artifacts/download-staged-artifact.sh
@@ -1,16 +1,21 @@
+#!/bin/bash
+
 # Downloads the artifacts for the specified version from the staging bucket for local testing.
 # Usage: download-staged-artifact.sh <version>
 # Example: download-staged-artifact.sh 0.1.0
-
 # Retrieve version from the first argument
+
 VERSION=$1
 if [ -z "$VERSION" ]; then
     echo "Usage: $0 <version>"
     exit
 fi
 
+# Gets the directory of the script
+SCRIPT_DIR=$(dirname $0)
+
 # Download the artifacts for the specified version from the staging bucket
-DOWNLOAD_DIR=$VERSION-staged
+DOWNLOAD_DIR=$SCRIPT_DIR/$VERSION-staged
 rm -rf $DOWNLOAD_DIR
 mkdir -p $DOWNLOAD_DIR
 aws s3 cp s3://waveterm-github-artifacts/staging-w2/$VERSION/ $DOWNLOAD_DIR/ --recursive --profile $AWS_PROFILE
