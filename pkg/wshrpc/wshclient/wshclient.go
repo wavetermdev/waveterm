f7c9fd9a0238feb7b402e45c894552ae7f9bf11e
Evan Simkowitz <esimkowitz@users.noreply.github.com>
2024-08-28 13:21:08 -0700

    revert formatting for auto generated files

--- pkg/wshrpc/wshclient/wshclient.go
+++ pkg/wshrpc/wshclient/wshclient.go
@@ -6,9 +6,9 @@
 package wshclient
 
 import (
-	"github.com/wavetermdev/thenextwave/pkg/waveobj"
-	"github.com/wavetermdev/thenextwave/pkg/wshrpc"
 	"github.com/wavetermdev/thenextwave/pkg/wshutil"
+	"github.com/wavetermdev/thenextwave/pkg/wshrpc"
+	"github.com/wavetermdev/thenextwave/pkg/waveobj"
 )
 
 // command "announce", wshserver.AnnounceCommand
@@ -185,3 +185,5 @@ func TestCommand(w *wshutil.WshRpc, data string, opts *wshrpc.RpcOpts) error {
 	_, err := sendRpcRequestCallHelper[any](w, "test", data, opts)
 	return err
 }
+
+
