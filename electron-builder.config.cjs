a2695e8c08ebdbec0c40fc429d98c588e1930da0
Evan Simkowitz <esimkowitz@users.noreply.github.com>
2024-08-30 10:13:40 -0700

    Set up Windows build pipeline (#292)
    This adds a new job to the Build Helper pipeline for building for
    Windows. This includes code signing via DigiCert. Right now, we can only
    build for x64 on Windows as wavesrv fails to build for arm64 in the
    default runner and the Windows ARM runner images are missing a bunch of
    tooling.
    
    This also adds new separated arm64 and x64 for macOS for those who don't
    want to use the universal binary.
    
    This also improves the general code quality of the Taskfile.yml and the
    build-helper.yml files.

--- electron-builder.config.cjs
+++ electron-builder.config.cjs
@@ -7,76 +7,84 @@ const path = require("path");
  * @see https://www.electron.build/configuration/configuration
  */
 const config = {
-  appId: pkg.build.appId,
-  productName: pkg.productName,
-  artifactName: "${productName}-${platform}-${arch}-${version}.${ext}",
-  npmRebuild: false,
-  nodeGypRebuild: false,
-  electronCompile: false,
-  files: [
-    {
-      from: "./dist",
-      to: "./dist",
-      filter: ["**/*"],
-    },
-    {
-      from: ".",
-      to: ".",
-      filter: ["package.json"],
-    },
-    "!node_modules", // We don't need electron-builder to package in Node modules as Vite has already bundled any code that our program is using.
-  ],
-  directories: {
-    output: "make",
-  },
-  asarUnpack: [
-    "dist/bin/**/*", // wavesrv and wsh binaries
-  ],
-  mac: {
-    target: [
-      {
-        target: "zip",
-        arch: "universal",
-      },
-      {
-        target: "dmg",
-        arch: "universal",
-      },
+    appId: pkg.build.appId,
+    productName: pkg.productName,
+    artifactName: "${productName}-${platform}-${arch}-${version}.${ext}",
+    npmRebuild: false,
+    nodeGypRebuild: false,
+    electronCompile: false,
+    files: [
+        {
+            from: "./dist",
+            to: "./dist",
+            filter: ["**/*"],
+        },
+        {
+            from: ".",
+            to: ".",
+            filter: ["package.json"],
+        },
+        "!node_modules", // We don't need electron-builder to package in Node modules as Vite has already bundled any code that our program is using.
     ],
-    icon: "build/icons.icns",
-    category: "public.app-category.developer-tools",
-    minimumSystemVersion: "10.15.0",
-    notarize: process.env.APPLE_TEAM_ID
-      ? {
-          teamId: process.env.APPLE_TEAM_ID,
-        }
-      : false,
-    binaries: fs
-      .readdirSync("dist/bin", { recursive: true, withFileTypes: true })
-      .filter((f) => f.isFile() && (f.name.startsWith("wavesrv") || f.name.includes("darwin")))
-      .map((f) => path.resolve(f.path, f.name)),
-  },
-  linux: {
-    executableName: pkg.productName,
-    category: "TerminalEmulator",
-    icon: "build/icons.icns",
-    target: ["zip", "deb", "rpm", "AppImage", "pacman"],
-    synopsis: pkg.description,
-    description: null,
-    desktop: {
-      Name: pkg.productName,
-      Comment: pkg.description,
-      Keywords: "developer;terminal;emulator;",
-      category: "Development;Utility;",
+    directories: {
+        output: "make",
     },
-  },
-  appImage: {
-    license: "LICENSE",
-  },
-  publish: {
-    provider: "generic",
-    url: "https://dl.waveterm.dev/releases-w2",
-  },
+    asarUnpack: [
+        "dist/bin/**/*", // wavesrv and wsh binaries
+    ],
+    mac: {
+        target: [
+            {
+                target: "zip",
+                arch: ["universal", "arm64", "x64"],
+            },
+            {
+                target: "dmg",
+                arch: ["universal", "arm64", "x64"],
+            },
+        ],
+        icon: "build/icons.icns",
+        category: "public.app-category.developer-tools",
+        minimumSystemVersion: "10.15.0",
+        notarize: process.env.APPLE_TEAM_ID
+            ? {
+                  teamId: process.env.APPLE_TEAM_ID,
+              }
+            : false,
+        binaries: fs
+            .readdirSync("dist/bin", { recursive: true, withFileTypes: true })
+            .filter((f) => f.isFile() && (f.name.startsWith("wavesrv") || f.name.includes("darwin")))
+            .map((f) => path.resolve(f.parentPath, f.name)),
+    },
+    linux: {
+        executableName: pkg.productName,
+        category: "TerminalEmulator",
+        icon: "build/icons.icns",
+        target: ["zip", "deb", "rpm", "AppImage", "pacman"],
+        synopsis: pkg.description,
+        description: null,
+        desktop: {
+            Name: pkg.productName,
+            Comment: pkg.description,
+            Keywords: "developer;terminal;emulator;",
+            category: "Development;Utility;",
+        },
+    },
+    win: {
+        icon: "build/icons.icns",
+        publisherName: "Command Line, Inc.",
+        target: ["nsis", "msi", "zip"],
+        certificateSubjectName: "Command Line Inc",
+        certificateSha1: process.env.SM_CODE_SIGNING_CERT_SHA1_HASH,
+        signingHashAlgorithms: ["sha256"],
+    },
+    appImage: {
+        license: "LICENSE",
+    },
+    publish: {
+        provider: "generic",
+        url: "https://dl.waveterm.dev/releases-w2",
+    },
 };
 
 module.exports = config;
