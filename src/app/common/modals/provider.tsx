a221655027fccfe6e9eb6e13bfad774461c82dd4
Red J Adaya <red.adaya@gmail.com>
2024-09-21 05:06:51 +0800

    new wave modal (#781)

--- src/app/common/modals/provider.tsx
+++ src/app/common/modals/provider.tsx
@@ -5,14 +5,34 @@ import * as React from "react";
 import * as mobxReact from "mobx-react";
 import { GlobalModel } from "@/models";
 import { TosModal } from "./tos";
+import { NewWaveModal } from "./newwave";
+
+const SessionStorageKey = "newWaveRendered";
 
 @mobxReact.observer
-class ModalsProvider extends React.Component {
+class ModalsProvider extends React.Component<{}, {}> {
+    constructor(props) {
+        super(props);
+        this.handleNewWaveOnClose = this.handleNewWaveOnClose.bind(this);
+    }
+
+    handleNewWaveOnClose() {
+        sessionStorage.setItem(SessionStorageKey, "1");
+        this.forceUpdate();
+    }
+
     render() {
         let store = GlobalModel.modalsModel.store.slice();
+
         if (GlobalModel.needsTos()) {
             return <TosModal />;
         }
+
+        const newWaveRendered = sessionStorage.getItem(SessionStorageKey);
+        if (!newWaveRendered) {
+            return <NewWaveModal onClose={this.handleNewWaveOnClose} />;
+        }
+
         let rtn: JSX.Element[] = [];
         for (let i = 0; i < store.length; i++) {
             let entry = store[i];
