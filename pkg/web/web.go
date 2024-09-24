11dccb3177d2e553acbc22d06f6010aca5542861
Evan Simkowitz <esimkowitz@users.noreply.github.com>
2024-09-24 16:19:44 -0700

    one more

--- pkg/web/web.go
+++ pkg/web/web.go
@@ -14,6 +14,7 @@ import (
 	"net"
 	"net/http"
 	"os"
+	"path/filepath"
 	"runtime/debug"
 	"strconv"
 	"time"
@@ -223,7 +224,7 @@ func handleLocalStreamFile(w http.ResponseWriter, r *http.Request, fileName stri
 		// use the custom response writer
 		rw := &notFoundBlockingResponseWriter{w: w, headers: http.Header{}}
 		// Serve the file using http.ServeFile
-		http.ServeFile(rw, r, fileName)
+		http.ServeFile(rw, r, filepath.Clean(fileName))
 		// if the file was not found, serve the transparent GIF
 		log.Printf("got streamfile status: %d\n", rw.status)
 		if rw.status == http.StatusNotFound {
