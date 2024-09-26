0c16b1a0d0722fdd72b76e8a91e62fdc2d60ca76
Evan Simkowitz <esimkowitz@users.noreply.github.com>
2024-09-26 14:14:14 -0700

    Storybook page, exclude Jotai from dependabot, use NodeJS 20 for builds

---
+++ .storybook/theme.ts
@@ -0,0 +1,9 @@
+import { create } from "@storybook/theming";
+
+export default create({
+    base: "light",
+    brandTitle: "Wave Terminal Storybook",
+    brandUrl: "https://www.waveterm.dev",
+    brandImage: "../assets/wave-light.png",
+    brandTarget: "_self",
+});
