061949354c23d3549e50203af1b5ef422157b3ce
Evan Simkowitz <esimkowitz@users.noreply.github.com>
2024-09-26 18:58:25 -0700

    Storybook with dark mode support (#880)

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -10,6 +10,8 @@ const config: StorybookConfig = {
         "@storybook/addon-essentials",
         "@chromatic-com/storybook",
         "@storybook/addon-interactions",
+        "storybook-dark-mode",
+        "./custom-addons/theme/register",
     ],
 
     core: {},
