5ecb2853397a94d5b40d200709854d9f958d441d
Red J Adaya <red.adaya@gmail.com>
2024-09-27 09:21:28 +0800

    menu (#846)

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -3,6 +3,9 @@ import type { Preview } from "@storybook/react";
 import React from "react";
 import { DndProvider } from "react-dnd";
 import { HTML5Backend } from "react-dnd-html5-backend";
+import "../frontend/app/theme.less";
+import "../frontend/app/app.less";
+import "../frontend/app/reset.less";
 import "./global.css";
 
 const preview: Preview = {
