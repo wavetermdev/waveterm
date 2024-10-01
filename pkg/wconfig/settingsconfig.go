dd2d1e2a8ce1b1697e019848ec131feae8ecf076
sawka <mike@commandline.dev>
2024-09-30 21:18:31 -0700

    add term:localshellopts

--- pkg/wconfig/settingsconfig.go
+++ pkg/wconfig/settingsconfig.go
@@ -48,11 +48,12 @@ type SettingsType struct {
 	AiMaxTokens float64 `json:"ai:maxtokens,omitempty"`
 	AiTimeoutMs float64 `json:"ai:timeoutms,omitempty"`
 
-	TermClear          bool    `json:"term:*,omitempty"`
-	TermFontSize       float64 `json:"term:fontsize,omitempty"`
-	TermFontFamily     string  `json:"term:fontfamily,omitempty"`
-	TermDisableWebGl   bool    `json:"term:disablewebgl,omitempty"`
-	TermLocalShellPath string  `json:"term:localshellpath,omitempty"`
+	TermClear          bool     `json:"term:*,omitempty"`
+	TermFontSize       float64  `json:"term:fontsize,omitempty"`
+	TermFontFamily     string   `json:"term:fontfamily,omitempty"`
+	TermDisableWebGl   bool     `json:"term:disablewebgl,omitempty"`
+	TermLocalShellPath string   `json:"term:localshellpath,omitempty"`
+	TermLocalShellOpts []string `json:"term:localshellopts,omitempty"`
 
 	EditorMinimapEnabled      bool `json:"editor:minimapenabled,omitempty"`
 	EditorStickyScrollEnabled bool `json:"editor:stickyscrollenabled,omitempty"`
