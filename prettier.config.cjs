8d3bafa1ce2fee269357df0742e53648824860df
Sylvia Crowe <software@oneirocosm.com>
2024-10-01 12:12:56 -0700

    fix: use prettier to format line endings
    This changes git to ignore line endings and prettier to set them. This
    should result in autogenerated files not being updated, while generated
    files get updated properly.

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -3,9 +3,10 @@ module.exports = {
     plugins: ["prettier-plugin-jsdoc", "prettier-plugin-organize-imports"],
     printWidth: 120,
     trailingComma: "es5",
-	useTabs: false,
+    useTabs: false,
     jsdocVerticalAlignment: true,
     jsdocSeparateReturnsFromParam: true,
     jsdocSeparateTagGroups: true,
     jsdocPreferCodeFences: true,
+    endOfLine: "lf",
 };
