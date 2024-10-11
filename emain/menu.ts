09db2030514832b95e630b3ec22a8776f3f91688
sawka <mike@commandline.dev>
2024-10-11 09:58:59 -0700

    devtools windows accel

--- emain/menu.ts
+++ emain/menu.ts
@@ -118,6 +118,7 @@ function getAppMenu(callbacks: AppMenuCallbacks): Electron.Menu {
         },
     ];
 
+    const devToolsAccel = unamePlatform === "darwin" ? "Option+Command+I" : "Alt+Meta+I";
     const viewMenu: Electron.MenuItemConstructorOptions[] = [
         {
             label: "Reload Tab",
@@ -144,7 +145,7 @@ function getAppMenu(callbacks: AppMenuCallbacks): Electron.Menu {
         },
         {
             label: "Toggle DevTools",
-            accelerator: "Option+Command+I", // TODO fix for windows
+            accelerator: devToolsAccel,
             click: () => {
                 const win = getFocusedWaveWindow();
                 if (win == null) {
