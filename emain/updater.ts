38ff2c18f1a1e4fd8f4f6f9091f66b00ccf688c1
Evan Simkowitz <esimkowitz@users.noreply.github.com>
2024-10-10 20:34:17 -0400

    Prevent the app from downgrading when a user installs beta over latest

--- emain/updater.ts
+++ emain/updater.ts
@@ -23,7 +23,8 @@ function getUpdateChannel(settings: SettingsType): string {
     let retVal = settingsChannel;
 
     // If the user setting doesn't exist yet, set it to the value of the updater config.
-    if (!settingsChannel) {
+    // If the user was previously on the `latest` channel and has downloaded a `beta` version, update their configured channel to `beta` to prevent downgrading.
+    if (!settingsChannel || (settingsChannel == "latest" && updaterChannel == "beta")) {
         console.log("Update channel setting does not exist, setting to value from updater config.");
         RpcApi.SetConfigCommand(ElectronWshClient, { "autoupdate:channel": updaterChannel });
         retVal = updaterChannel;
@@ -57,6 +58,7 @@ export class Updater {
 
         // Only update the release channel if it's specified, otherwise use the one configured in the updater.
         autoUpdater.channel = getUpdateChannel(settings);
+        autoUpdater.allowDowngrade = false;
 
         autoUpdater.removeAllListeners();
 
