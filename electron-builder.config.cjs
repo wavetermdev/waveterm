6ee3dad33aed336f4b5d0699ad390ebc9f20e7b8
Ian Jennings <ian@meetjennings.com>
2024-10-14 19:08:51 -0500

    drop csc fields

--- electron-builder.config.cjs
+++ electron-builder.config.cjs
@@ -75,10 +75,7 @@ const config = {
     win: {
         icon: "build/icons.icns",
         publisherName: "Command Line Inc",
-        target: ["nsis", "msi", "zip"],
-        certificateSubjectName: "Command Line Inc",
-        certificateSha1: process.env.SM_CODE_SIGNING_CERT_SHA1_HASH,
-        signingHashAlgorithms: ["sha256"],
+        target: ["nsis", "msi", "zip"]
     },
     appImage: {
         license: "LICENSE",
