0e72d7fd482319781a4a8fb4e76b06a6f48d2e4e
sawka <mike@commandline.dev>
2024-10-18 11:04:53 -0700

    remove comment

--- pkg/web/ws.go
+++ pkg/web/ws.go
@@ -252,7 +252,7 @@ func registerConn(wsConnId string, routeId string, wproxy *wshutil.WshRpcProxy) 
 		wshutil.DefaultRouter.UnregisterRoute(routeId)
 	}
 	RouteToConnMap[routeId] = wsConnId
-	wshutil.DefaultRouter.RegisterRoute(routeId, wproxy) // better to register
+	wshutil.DefaultRouter.RegisterRoute(routeId, wproxy)
 }
 
 func unregisterConn(wsConnId string, routeId string) {
