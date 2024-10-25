ca382b19b11042d0b67e83d29cc2d941b92693db
sawka <mike@commandline.dev>
2024-10-25 13:42:58 -0700

    implement VDom background switcher

--- cmd/wsh/cmd/wshcmd-html.go
+++ cmd/wsh/cmd/wshcmd-html.go
@@ -4,16 +4,21 @@
 package cmd
 
 import (
+	"context"
 	"log"
 	"time"
 
 	"github.com/spf13/cobra"
 	"github.com/wavetermdev/waveterm/pkg/vdom"
 	"github.com/wavetermdev/waveterm/pkg/vdom/vdomclient"
+	"github.com/wavetermdev/waveterm/pkg/waveobj"
+	"github.com/wavetermdev/waveterm/pkg/wshrpc"
+	"github.com/wavetermdev/waveterm/pkg/wshrpc/wshclient"
 	"github.com/wavetermdev/waveterm/pkg/wshutil"
 )
 
 var htmlCmdNewBlock bool
+var GlobalVDomClient *vdomclient.Client
 
 func init() {
 	htmlCmd.Flags().BoolVarP(&htmlCmdNewBlock, "newblock", "n", false, "create a new block")
@@ -27,16 +32,116 @@ var htmlCmd = &cobra.Command{
 	RunE:   htmlRun,
 }
 
+func StyleTag(ctx context.Context, props map[string]any) any {
+	return vdom.Bind(`
+	<style>
+	.root {
+	    padding: 10px;
+	}
+
+	.background {
+	    display: flex;
+    	align-items: center;
+	    width: 100%;
+
+	    .background-inner {
+    	    max-width: 300px;
+
+        	.bg-item {
+	            cursor: pointer;
+    	        padding: 8px 12px;
+        	    border-radius: 4px;
+            	display: flex;
+	            flex-direction: row;
+    	        align-items: flex-start;
+        	    justify-content: flex-start;
+
+	            &:hover {
+    	            background-color: var(--button-grey-hover-bg);
+        	    }
+
+            	.bg-preview {
+	                width: 20px;
+    	            height: 20px;
+        	        margin-right: 10px;
+            	    border-radius: 50%;
+					border: 1px solid #777;
+	            }
+
+    	        .bg-label {
+				    display: block;
+				    white-space: nowrap;
+				    overflow: hidden;
+				    text-overflow: ellipsis;	            
+				}
+    	    }
+	    }
+	}
+	</style>
+	`, nil)
+}
+
+func BgItemTag(ctx context.Context, props map[string]any) any {
+	clickFn := func() {
+		log.Printf("bg item clicked %q\n", props["bg"])
+		blockInfo, err := wshclient.BlockInfoCommand(GlobalVDomClient.RpcClient, GlobalVDomClient.RpcContext.BlockId, nil)
+		if err != nil {
+			log.Printf("error getting block info: %v\n", err)
+			return
+		}
+		log.Printf("block info: tabid=%q\n", blockInfo.TabId)
+		err = wshclient.SetMetaCommand(GlobalVDomClient.RpcClient, wshrpc.CommandSetMetaData{
+			ORef: waveobj.ORef{OType: "tab", OID: blockInfo.TabId},
+			Meta: map[string]any{"bg": props["bg"]},
+		}, nil)
+		if err != nil {
+			log.Printf("error setting meta: %v\n", err)
+		}
+		// wshclient.SetMetaCommand(GlobalVDomClient.RpcClient)
+	}
+	params := map[string]any{
+		"bg":           props["bg"],
+		"label":        props["label"],
+		"clickHandler": clickFn,
+	}
+	return vdom.Bind(`
+        <div className="bg-item" onClick="#param:clickHandler">
+            <div className="bg-preview" style="background: #param:bg"></div>
+            <div className="bg-label"><bindparam key="label"/></div>
+        </div>`, params)
+}
+
+func AllBgItemsTag(ctx context.Context, props map[string]any) any {
+	items := []map[string]any{
+		{"bg": nil, "label": "default"},
+		{"bg": "#ff0000", "label": "red"},
+		{"bg": "#00ff00", "label": "green"},
+		{"bg": "#0000ff", "label": "blue"},
+	}
+	bgElems := make([]*vdom.VDomElem, 0)
+	for _, item := range items {
+		elem := vdom.E("BgItemTag", item)
+		bgElems = append(bgElems, elem)
+	}
+	return vdom.Bind(`
+	<div className="background">
+		<div className="background-inner">
+			<bindparam key="bgElems"/>
+		</div>
+	</div>
+	`, map[string]any{"bgElems": bgElems})
+}
+
 func MakeVDom() *vdom.VDomElem {
 	vdomStr := `
-	<div>
-	  <h1 style="color:red; background-color: #bind:$.bgcolor; border-radius: 4px; padding: 5px;">hello vdom world</h1>
-	  <div><bind key="$.text"/> | num[<bind key="$.num"/>]</div>
+	<div className="root">
+	  <StyleTag/>
+	  <h1>Set Background</h1>
 	  <div>
-	    <button data-text="hello" onClick='#globalevent:clickinc'>increment</button>
+	      <wave:markdown text="*quick vdom application to set background colors*"/>
 	  </div>
 	  <div>
-	      <wave:markdown text="*hello from markdown*"/>
+	      <AllBgItemsTag/>
 	  </div>
 	</div>
 	`
@@ -58,11 +163,15 @@ func htmlRun(cmd *cobra.Command, args []string) error {
 	if err != nil {
 		return err
 	}
+	GlobalVDomClient = client
 	client.SetGlobalEventHandler(GlobalEventHandler)
 	log.Printf("created client: %v\n", client)
 	client.SetAtomVal("bgcolor", "#0000ff77")
 	client.SetAtomVal("text", "initial text")
 	client.SetAtomVal("num", 0)
+	client.RegisterComponent("StyleTag", StyleTag)
+	client.RegisterComponent("BgItemTag", BgItemTag)
+	client.RegisterComponent("AllBgItemsTag", AllBgItemsTag)
 	client.SetRootElem(MakeVDom())
 	err = client.CreateVDomContext(&vdom.VDomTarget{NewBlock: htmlCmdNewBlock})
 	if err != nil {
