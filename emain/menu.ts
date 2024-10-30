a5c71ec214539423788db8464fff05e986551c93
Evan Simkowitz <esimkowitz@users.noreply.github.com>
2024-10-30 13:40:48 -0700

    test

--- emain/menu.ts
+++ emain/menu.ts
@@ -118,7 +118,7 @@ function getAppMenu(callbacks: AppMenuCallbacks): Electron.Menu {
         },
     ];
 
-    const devToolsAccel = unamePlatform === "darwin" ? "Option+Command+I" : "Alt+Super+I";
+    const devToolsAccel: string = unamePlatform === "darwin" ? "Option+Command+I" : "Alt+Super+I";
     const viewMenu: Electron.MenuItemConstructorOptions[] = [
         {
             label: "Reload Tab",
