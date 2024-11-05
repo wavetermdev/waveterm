f46f5dac3c3388341398632067939ff59bb64388
Evan Simkowitz <esimkowitz@users.noreply.github.com>
2024-11-05 12:35:17 -0800

    Move docsite to main repo

---
+++ docs/sidebars.ts
@@ -0,0 +1,48 @@
+import type { SidebarsConfig } from "@docusaurus/plugin-content-docs";
+
+const sidebars: SidebarsConfig = {
+    mainSidebar: [
+        {
+            type: "doc",
+            id: "index",
+            label: "Home",
+        },
+        {
+            type: "doc",
+            id: "keybindings",
+            label: "Keybindings",
+        },
+        {
+            type: "doc",
+            id: "config",
+            label: "Configuration",
+        },
+        {
+            type: "doc",
+            id: "customwidgets",
+            label: "Custom Widgets",
+        },
+        {
+            type: "doc",
+            id: "telemetry",
+            label: "Telemetry",
+        },
+        {
+            type: "doc",
+            id: "wsh",
+            label: "Wsh",
+        },
+        {
+            type: "doc",
+            id: "widgets",
+            label: "Widgets",
+        },
+        {
+            type: "doc",
+            id: "faq",
+            label: "FAQ",
+        },
+    ],
+};
+
+export default sidebars;
