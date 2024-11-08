d5297cc5d4194125ab0183bcc96829a7a3743e05
Mike Sawka <mike@commandline.dev>
2024-11-08 11:08:52 -0800

    wsh editconfig (#1243)

--- cmd/wsh/cmd/wshcmd-version.go
+++ cmd/wsh/cmd/wshcmd-version.go
@@ -4,6 +4,7 @@
 package cmd
 
 import (
+	"encoding/json"
 	"fmt"
 
 	"github.com/spf13/cobra"
@@ -14,36 +15,59 @@ import (
 )
 
 var versionVerbose bool
+var versionJSON bool
 
 // versionCmd represents the version command
 var versionCmd = &cobra.Command{
-	Use:   "version [-v]",
+	Use:   "version [-v] [--json]",
 	Short: "Print the version number of wsh",
 	RunE:  runVersionCmd,
 }
 
 func init() {
 	versionCmd.Flags().BoolVarP(&versionVerbose, "verbose", "v", false, "Display full version information")
+	versionCmd.Flags().BoolVar(&versionJSON, "json", false, "Output version information in JSON format")
 	rootCmd.AddCommand(versionCmd)
 }
 
 func runVersionCmd(cmd *cobra.Command, args []string) error {
-	if !versionVerbose {
+	if !versionVerbose && !versionJSON {
 		WriteStdout("wsh v%s\n", wavebase.WaveVersion)
 		return nil
 	}
+
 	err := preRunSetupRpcClient(cmd, args)
 	if err != nil {
 		return err
 	}
+
 	resp, err := wshclient.WaveInfoCommand(RpcClient, &wshrpc.RpcOpts{Timeout: 2000})
 	if err != nil {
 		return err
 	}
+
 	updateChannel, err := wshclient.GetUpdateChannelCommand(RpcClient, &wshrpc.RpcOpts{Timeout: 2000, Route: wshutil.ElectronRoute})
 	if err != nil {
 		return err
 	}
+
+	if versionJSON {
+		info := map[string]interface{}{
+			"version":       resp.Version,
+			"buildtime":     resp.BuildTime,
+			"configdir":     resp.ConfigDir,
+			"datadir":       resp.DataDir,
+			"updatechannel": updateChannel,
+		}
+		outBArr, err := json.MarshalIndent(info, "", "  ")
+		if err != nil {
+			return fmt.Errorf("formatting version info: %v", err)
+		}
+		WriteStdout("%s\n", string(outBArr))
+		return nil
+	}
+
+	// Default verbose text output
 	fmt.Printf("v%s (%s)\n", resp.Version, resp.BuildTime)
 	fmt.Printf("configdir: %s\n", resp.ConfigDir)
 	fmt.Printf("datadir:   %s\n", resp.DataDir)
