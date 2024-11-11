c46ffcafd67e5891ef1cf0df1ddc447fab34677a
Red Adaya <red.adaya@gmail.com>
2024-11-11 13:25:53 +0800

    save work

--- frontend/app/notification/notificationpopover.tsx
+++ frontend/app/notification/notificationpopover.tsx
@@ -28,51 +28,68 @@ const NotificationPopover = () => {
         setNotificationPopoverMode(!notificationPopoverMode);
     }, [notificationPopoverMode]);
 
+    const hasErrors = notifications.some((n) => n.color === "red");
+    const hasUpdate = notifications.some((n) => n.title.includes("Update"));
+
+    const addOnClassNames = hasUpdate ? "solid green" : hasErrors ? "solid red" : "ghost grey";
+
+    const getIcon = () => {
+        if (hasUpdate) {
+            return <i className={makeIconClass("arrows-rotate", false)}></i>;
+        }
+        return <i className={makeIconClass("bell", false)}></i>;
+    };
+
     return (
         <Popover className="notification-popover" placement="left-end" offset={{ mainAxis: 20, crossAxis: 2 }}>
             <PopoverButton
-                className="notification-trigger-button horizontal-padding-6 vertical-padding-4 border-radius-6"
+                className={clsx(
+                    "notification-trigger-button horizontal-padding-6 vertical-padding-4 border-radius-",
+                    addOnClassNames
+                )}
+                disabled={notifications.length === 0}
                 onClick={handleTogglePopover}
             >
-                <i className={makeIconClass("bell", false)}></i>
-                {/* {notifications.length > 0 && <span className="notification-count">{notifications.length}</span>} */}
+                {getIcon()}
             </PopoverButton>
-            <PopoverContent className="notification-content">
-                <div className="header">
-                    <span>Notifications</span>
-                    <Button
-                        className="ghost grey close-all-btn horizontal-padding-3 vertical-padding-3"
-                        onClick={(e) => {
-                            e.stopPropagation();
-                            removeAllNotifications();
-                        }}
+            {notifications.length > 0 && (
+                <PopoverContent className="notification-content">
+                    <div className="header">
+                        <span>Notifications</span>
+                        <Button
+                            className="ghost grey close-all-btn horizontal-padding-3 vertical-padding-3"
+                            onClick={(e) => {
+                                e.stopPropagation();
+                                removeAllNotifications();
+                            }}
+                        >
+                            Clear All
+                        </Button>
+                    </div>
+                    <OverlayScrollbarsComponent
+                        className="scrollable"
+                        options={{ scrollbars: { autoHide: "leave" } }}
+                        style={{ maxHeight: window.innerHeight / 2 }}
                     >
-                        Clear All
-                    </Button>
-                </div>
-                <OverlayScrollbarsComponent
-                    className="scrollable"
-                    options={{ scrollbars: { autoHide: "leave" } }}
-                    style={{ maxHeight: window.innerHeight / 2 }}
-                >
-                    {notifications.map((notif, index) => (
-                        <Fragment key={notif.id}>
-                            <NotificationItem
-                                className={clsx({ hovered: hoveredId === notif.id })}
-                                notification={notif}
-                                onRemove={removeNotification}
-                                onCopy={copyNotification}
-                                onActionClick={handleActionClick}
-                                formatTimestamp={formatTimestamp}
-                                isBubble={false}
-                                onMouseEnter={() => setHoveredId(notif.id)}
-                                onMouseLeave={() => setHoveredId(null)}
-                            />
-                            {index !== notifications.length - 1 && <div className="divider"></div>}
-                        </Fragment>
-                    ))}
-                </OverlayScrollbarsComponent>
-            </PopoverContent>
+                        {notifications.map((notif, index) => (
+                            <Fragment key={notif.id}>
+                                <NotificationItem
+                                    className={clsx({ hovered: hoveredId === notif.id })}
+                                    notification={notif}
+                                    onRemove={removeNotification}
+                                    onCopy={copyNotification}
+                                    onActionClick={handleActionClick}
+                                    formatTimestamp={formatTimestamp}
+                                    isBubble={false}
+                                    onMouseEnter={() => setHoveredId(notif.id)}
+                                    onMouseLeave={() => setHoveredId(null)}
+                                />
+                                {index !== notifications.length - 1 && <div className="divider"></div>}
+                            </Fragment>
+                        ))}
+                    </OverlayScrollbarsComponent>
+                </PopoverContent>
+            )}
         </Popover>
     );
 };
