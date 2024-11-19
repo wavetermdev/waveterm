1ff6a46a4f3c480ed6622180766228a9284bc30d
Evan Simkowitz <esimkowitz@users.noreply.github.com>
2024-11-18 16:46:23 -0800

    save

--- db/migrations-wstore/000006_workspace.up.sql
+++ db/migrations-wstore/000006_workspace.up.sql
@@ -1,4 +1,5 @@
 UPDATE db_workspace
-SET data = json_set(db_workspace.data, '$.activetabid', json_each(db_window.data, '$.activetabid'))
+SELECT value FROM json_each(db_window.data, '$.activetabid') as activetabid
+SET data = json_set(db_workspace.data, '$.activetabid', activetabid)
 FROM db_window
 WHERE db_workspace.oid IN (SELECT value FROM json_each(db_window.data, '$.workspaceid'));
