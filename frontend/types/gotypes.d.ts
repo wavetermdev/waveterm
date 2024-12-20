62dcf1326dd910b78472155feddec30bc669169e
Evan Simkowitz <esimkowitz@users.noreply.github.com>
2024-12-20 17:02:08 -0500

    Allow users to close windows without confirming, delete last window (#1598)
    Adds `window:savelastwindow` and `window:confirmclose` settings, which
    can be used to alter the default window close behavior. Both of these
    default to `true` to maintain the existing default behavior.
    
    If `window:savelastwindow` is unset, the same logic will be used as if
    the user had more than one window remaining (see below).
    
    If `window:confirmonclose` is unset, the user will no longer be prompted
    to confirm if they are closing a window whose workspace has unsaved
    changes (the workspace is not named and it has more than one tab).

--- frontend/types/gotypes.d.ts
+++ frontend/types/gotypes.d.ts
@@ -679,6 +679,8 @@ declare global {
         "window:magnifiedblocksize"?: number;
         "window:magnifiedblockblurprimarypx"?: number;
         "window:magnifiedblockblursecondarypx"?: number;
+        "window:confirmclose"?: boolean;
+        "window:savelastwindow"?: boolean;
         "telemetry:*"?: boolean;
         "telemetry:enabled"?: boolean;
         "conn:*"?: boolean;
