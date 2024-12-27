1aecb2cb40314a80efcc8c4a9e37b19adf837fd6
weilirs <weilirs@umich.edu>
2024-12-27 17:10:05 -0500

    feat: wsh export-config command

---
+++ cmd/wsh/cmd/wshcmd-export-config.go
@@ -0,0 +1,88 @@
+package cmd
+
+import (
+	"archive/zip"
+	"fmt"
+	"io"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/spf13/cobra"
+)
+
+func init() {
+	var exportConfigCmd = &cobra.Command{
+		Use:   "export-config [output-path]",
+		Short: "export Wave Terminal configuration",
+		Long:  "Export Wave Terminal configuration files into a zip archive",
+		RunE:  runExportConfig,
+	}
+	rootCmd.AddCommand(exportConfigCmd)
+}
+
+func runExportConfig(cmd *cobra.Command, args []string) error {
+	if len(args) != 1 {
+		return fmt.Errorf("export-config requires an output path")
+	}
+
+	outputPath := args[0]
+	if !strings.HasSuffix(outputPath, ".zip") {
+		outputPath += ".zip"
+	}
+
+	configDir := getWaveConfigDir()
+	if err := zipDir(configDir, outputPath); err != nil {
+		return fmt.Errorf("export-config failed: %v", err)
+	}
+
+	fmt.Printf("Configuration exported to %s\n", outputPath)
+	return nil
+}
+
+func zipDir(sourceDir, zipPath string) error {
+	zipFile, err := os.Create(zipPath)
+	if err != nil {
+		return err
+	}
+	defer zipFile.Close()
+
+	archive := zip.NewWriter(zipFile)
+	defer archive.Close()
+
+	return filepath.Walk(sourceDir, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		if info.IsDir() {
+			return nil
+		}
+
+		relPath, err := filepath.Rel(sourceDir, path)
+		if err != nil {
+			return err
+		}
+
+		file, err := os.Open(path)
+		if err != nil {
+			return err
+		}
+		defer file.Close()
+
+		writer, err := archive.Create(relPath)
+		if err != nil {
+			return err
+		}
+
+		_, err = io.Copy(writer, file)
+		return err
+	})
+}
+
+func getWaveConfigDir() string {
+	homeDir, err := os.UserHomeDir()
+	if err != nil {
+		return ""
+	}
+	return filepath.Join(homeDir, ".config", "waveterm")
+}
