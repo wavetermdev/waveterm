e3da92e4e245f6da905efbba6636bb8d11061260
Evan Simkowitz <esimkowitz@users.noreply.github.com>
2025-01-15 20:30:16 -0800

    Filter out wildcard patterns in `~/.ssh/config` for connections dropdown

--- pkg/remote/conncontroller/conncontroller.go
+++ pkg/remote/conncontroller/conncontroller.go
@@ -877,6 +877,9 @@ func resolveSshConfigPatterns(configFiles []string) ([]string, error) {
 			// for each host, find the first good alias
 			for _, hostPattern := range host.Patterns {
 				hostPatternStr := hostPattern.String()
+				if hostPatternStr == "" || strings.Contains(hostPatternStr, "*") {
+					continue
+				}
 				normalized := remote.NormalizeConfigPattern(hostPatternStr)
 				if !strings.Contains(hostPatternStr, "*") && !strings.Contains(hostPatternStr, "?") && !strings.Contains(hostPatternStr, "!") && !alreadyUsed[normalized] {
 					discoveredPatterns = append(discoveredPatterns, normalized)
