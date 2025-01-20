52cc8ab1cdb5f2b223c0918522d07469f18b6ca3
sawka <mike@commandline.dev>
2025-01-19 19:34:24 -0800

    add more files for tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: [
-        "./index.html",
-        "./frontend/**/*.{js,ts,jsx,tsx,scss,css}", // adjust this path if your frontend files are elsewhere
-    ],
+    content: ["./index.html", "./frontend/**/*.{js,ts,jsx,tsx,scss,css}", "./.storybook/**/*.{js,ts,jsx,tsx,scss,css}"],
     theme: {
         extend: {},
     },
