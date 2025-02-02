1d804fa7eafcc8448e42716c5faab9f7e0339803
Evan Simkowitz <esimkowitz@users.noreply.github.com>
2025-02-01 16:14:47 -0800

    more impls

--- pkg/remote/fileshare/s3fs/s3fs.go
+++ pkg/remote/fileshare/s3fs/s3fs.go
@@ -4,9 +4,12 @@
 package s3fs
 
 import (
+	"bytes"
 	"context"
+	"encoding/base64"
 	"errors"
 	"fmt"
+	"io"
 	"log"
 	"regexp"
 	"strings"
@@ -14,6 +17,7 @@ import (
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/service/s3"
 	"github.com/aws/aws-sdk-go-v2/service/s3/types"
+	"github.com/aws/smithy-go"
 	"github.com/wavetermdev/waveterm/pkg/remote/awsconn"
 	"github.com/wavetermdev/waveterm/pkg/remote/connparse"
 	"github.com/wavetermdev/waveterm/pkg/remote/fileshare/fstype"
@@ -35,11 +39,54 @@ func NewS3Client(config *aws.Config) *S3Client {
 }
 
 func (c S3Client) Read(ctx context.Context, conn *connparse.Connection, data wshrpc.FileData) (*wshrpc.FileData, error) {
-	return nil, errors.ErrUnsupported
+	rtnCh := c.ReadStream(ctx, conn, data)
+	var fileData *wshrpc.FileData
+	firstPk := true
+	isDir := false
+	var fileBuf bytes.Buffer
+	for respUnion := range rtnCh {
+		if respUnion.Error != nil {
+			return nil, respUnion.Error
+		}
+		resp := respUnion.Response
+		if firstPk {
+			firstPk = false
+			// first packet has the fileinfo
+			if resp.Info == nil {
+				return nil, fmt.Errorf("stream file protocol error, first pk fileinfo is empty")
+			}
+			fileData = &resp
+			if fileData.Info.IsDir {
+				isDir = true
+			}
+			continue
+		}
+		if isDir {
+			if len(resp.Entries) == 0 {
+				continue
+			}
+			fileData.Entries = append(fileData.Entries, resp.Entries...)
+		} else {
+			if resp.Data64 == "" {
+				continue
+			}
+			decoder := base64.NewDecoder(base64.StdEncoding, bytes.NewReader([]byte(resp.Data64)))
+			_, err := io.Copy(&fileBuf, decoder)
+			if err != nil {
+				return nil, fmt.Errorf("stream file, failed to decode base64 data: %w", err)
+			}
+		}
+	}
+	if !isDir {
+		fileData.Data64 = base64.StdEncoding.EncodeToString(fileBuf.Bytes())
+	}
+	return fileData, nil
 }
 
 func (c S3Client) ReadStream(ctx context.Context, conn *connparse.Connection, data wshrpc.FileData) <-chan wshrpc.RespOrErrorUnion[wshrpc.FileData] {
-	if conn.Host == "" || conn.Host == "/" {
+	bucket := conn.Host
+	objectKey := conn.Path
+	if bucket == "" || bucket == "/" || objectKey == "" || objectKey == "/" {
 		rtn := make(chan wshrpc.RespOrErrorUnion[wshrpc.FileData], 1)
 		defer close(rtn)
 		entries, err := c.ListEntries(ctx, conn, nil)
@@ -50,9 +97,60 @@ func (c S3Client) ReadStream(ctx context.Context, conn *connparse.Connection, da
 		rtn <- wshrpc.RespOrErrorUnion[wshrpc.FileData]{Response: wshrpc.FileData{Entries: entries}}
 		return rtn
 	} else {
-		rtn := make(chan wshrpc.RespOrErrorUnion[wshrpc.FileData], 1)
-		defer close(rtn)
-		rtn <- wshutil.RespErr[wshrpc.FileData](errors.ErrUnsupported)
+		rtn := make(chan wshrpc.RespOrErrorUnion[wshrpc.FileData], 16)
+		go func() {
+			defer close(rtn)
+			result, err := c.client.GetObject(ctx, &s3.GetObjectInput{
+				Bucket: &bucket,
+				Key:    &objectKey,
+			})
+			if err != nil {
+				var noKey *types.NoSuchKey
+				if errors.As(err, &noKey) {
+					log.Printf("Can't get object %s from bucket %s. No such key exists.\n", objectKey, bucket)
+					err = noKey
+				} else {
+					log.Printf("Couldn't get object %v:%v. Here's why: %v\n", bucket, objectKey, err)
+				}
+				rtn <- wshutil.RespErr[wshrpc.FileData](err)
+				return
+			}
+			size := int64(0)
+			if result.ContentLength != nil {
+				size = *result.ContentLength
+			}
+			rtn <- wshrpc.RespOrErrorUnion[wshrpc.FileData]{Response: wshrpc.FileData{Info: &wshrpc.FileInfo{
+				Name:    objectKey,
+				IsDir:   false,
+				Size:    size,
+				ModTime: result.LastModified.UnixMilli(),
+				Path:    conn.GetFullURI(),
+			}}}
+			if size == 0 {
+				return
+			}
+			defer result.Body.Close()
+			for {
+				select {
+				case <-ctx.Done():
+					return
+				default:
+					buf := make([]byte, wshrpc.FileChunkSize)
+					n, err := result.Body.Read(buf)
+					if err != nil {
+						if err.Error() == "EOF" {
+							break
+						}
+						rtn <- wshutil.RespErr[wshrpc.FileData](err)
+						return
+					}
+					if n == 0 {
+						break
+					}
+					rtn <- wshrpc.RespOrErrorUnion[wshrpc.FileData]{Response: wshrpc.FileData{Data64: base64.StdEncoding.EncodeToString(buf[:n])}}
+				}
+			}
+		}()
 		return rtn
 	}
 }
@@ -121,21 +219,11 @@ func (c S3Client) ListEntriesStream(ctx context.Context, conn *connparse.Connect
 				Prefix: aws.String(conn.Path),
 			}
 			objectPaginator := s3.NewListObjectsV2Paginator(c.client, input)
-			var parentPath string
-			hostAndPath := conn.GetPathWithHost()
-			slashIndices := slashRe.FindAllStringIndex(hostAndPath, -1)
-			if slashIndices != nil && len(slashIndices) > 0 {
-				if slashIndices[len(slashIndices)-1][0] != len(hostAndPath)-1 {
-					parentPath = hostAndPath[:slashIndices[len(slashIndices)-1][0]+1]
-				} else if len(slashIndices) > 1 {
-					parentPath = hostAndPath[:slashIndices[len(slashIndices)-2][0]+1]
-				}
-			}
-
+			parentPath := getParentPathUri(conn)
 			if parentPath != "" {
 				rtn <- wshrpc.RespOrErrorUnion[wshrpc.CommandRemoteListEntriesRtnData]{Response: wshrpc.CommandRemoteListEntriesRtnData{FileInfo: []*wshrpc.FileInfo{
 					{
-						Path:  fmt.Sprintf("%s://%s", conn.Scheme, parentPath),
+						Path:  parentPath,
 						Name:  "..",
 						IsDir: true,
 						Size:  -1,
@@ -223,23 +311,112 @@ func (c S3Client) ListEntriesStream(ctx context.Context, conn *connparse.Connect
 }
 
 func (c S3Client) Stat(ctx context.Context, conn *connparse.Connection) (*wshrpc.FileInfo, error) {
-	return nil, errors.ErrUnsupported
+	bucketName := conn.Host
+	objectKey := conn.Path
+	if bucketName == "" || bucketName == "/" {
+		return &wshrpc.FileInfo{
+			Name:    "/",
+			IsDir:   true,
+			Size:    -1,
+			ModTime: 0,
+		}, nil
+	}
+	if objectKey == "" || objectKey == "/" {
+		_, err := c.client.HeadBucket(ctx, &s3.HeadBucketInput{
+			Bucket: aws.String(bucketName),
+		})
+		exists := true
+		if err != nil {
+			var apiError smithy.APIError
+			if errors.As(err, &apiError) {
+				switch apiError.(type) {
+				case *types.NotFound:
+					log.Printf("Bucket %v is available.\n", bucketName)
+					exists = false
+					err = nil
+				default:
+					log.Printf("Either you don't have access to bucket %v or another error occurred. "+
+						"Here's what happened: %v\n", bucketName, err)
+				}
+			}
+		} else {
+			log.Printf("Bucket %v exists and you already own it.", bucketName)
+		}
+
+		if exists {
+			return &wshrpc.FileInfo{
+				Name:    bucketName,
+				IsDir:   true,
+				Size:    -1,
+				ModTime: 0,
+			}, nil
+		} else {
+			return nil, fmt.Errorf("bucket %v does not exist", bucketName)
+		}
+	}
+	result, err := c.client.HeadObject(ctx, &s3.HeadObjectInput{
+		Bucket: aws.String(bucketName),
+		Key:    aws.String(objectKey),
+	})
+	if err != nil {
+		var noKey *types.NoSuchKey
+		if errors.As(err, &noKey) {
+			log.Printf("Can't get object %s from bucket %s. No such key exists.\n", objectKey, bucketName)
+			err = noKey
+		} else {
+			log.Printf("Couldn't get object %v:%v. Here's why: %v\n", bucketName, objectKey, err)
+		}
+		return nil, err
+	}
+	size := int64(0)
+	if result.ContentLength != nil {
+		size = *result.ContentLength
+	}
+	lastModified := int64(0)
+	if result.LastModified != nil {
+		lastModified = result.LastModified.UnixMilli()
+	}
+	return &wshrpc.FileInfo{
+		Name:    objectKey,
+		Path:    conn.GetFullURI(),
+		Dir:     getParentPathUri(conn),
+		IsDir:   false,
+		Size:    size,
+		ModTime: lastModified,
+	}, nil
 }
 
 func (c S3Client) PutFile(ctx context.Context, conn *connparse.Connection, data wshrpc.FileData) error {
-	return errors.ErrUnsupported
+	if data.At != nil {
+		return errors.Join(errors.ErrUnsupported, fmt.Errorf("file data offset and size not supported"))
+	}
+	bucket := conn.Host
+	objectKey := conn.Path
+	if bucket == "" || bucket == "/" || objectKey == "" || objectKey == "/" {
+		return errors.Join(errors.ErrUnsupported, fmt.Errorf("bucket and object key must be specified"))
+	}
+	_, err := c.client.PutObject(ctx, &s3.PutObjectInput{
+		Bucket: aws.String(bucket),
+		Key:    aws.String(objectKey),
+		Body:   bytes.NewReader([]byte(data.Data64)),
+	})
+	return err
 }
 
 func (c S3Client) AppendFile(ctx context.Context, conn *connparse.Connection, data wshrpc.FileData) error {
-	return errors.ErrUnsupported
+	return errors.Join(errors.ErrUnsupported, fmt.Errorf("append file not supported"))
 }
 
 func (c S3Client) Mkdir(ctx context.Context, conn *connparse.Connection) error {
-	return errors.ErrUnsupported
+	return errors.Join(errors.ErrUnsupported, fmt.Errorf("mkdir not supported"))
 }
 
 func (c S3Client) MoveInternal(ctx context.Context, srcConn, destConn *connparse.Connection, opts *wshrpc.FileCopyOpts) error {
-	return errors.ErrUnsupported
+	err := c.CopyInternal(ctx, srcConn, destConn, opts)
+	if err != nil {
+		return err
+	}
+	return c.Delete(ctx, srcConn, true)
 }
 
 func (c S3Client) CopyRemote(ctx context.Context, srcConn, destConn *connparse.Connection, srcClient fstype.FileShareClient, opts *wshrpc.FileCopyOpts) error {
@@ -247,17 +424,100 @@ func (c S3Client) CopyRemote(ctx context.Context, srcConn, destConn *connparse.C
 }
 
 func (c S3Client) CopyInternal(ctx context.Context, srcConn, destConn *connparse.Connection, opts *wshrpc.FileCopyOpts) error {
-	return errors.ErrUnsupported
+	srcBucket := srcConn.Host
+	srcKey := srcConn.Path
+	destBucket := destConn.Host
+	destKey := destConn.Path
+	if srcBucket == "" || srcBucket == "/" || srcKey == "" || srcKey == "/" || destBucket == "" || destBucket == "/" || destKey == "" || destKey == "/" {
+		return errors.Join(errors.ErrUnsupported, fmt.Errorf("source and destination bucket and object key must be specified"))
+	}
+	_, err := c.client.CopyObject(ctx, &s3.CopyObjectInput{
+		Bucket:     aws.String(destBucket),
+		Key:        aws.String(destKey),
+		CopySource: aws.String(fmt.Sprintf("%s/%s", srcBucket, srcKey)),
+	})
+	return err
 }
 
 func (c S3Client) Delete(ctx context.Context, conn *connparse.Connection, recursive bool) error {
-	return errors.ErrUnsupported
+	bucket := conn.Host
+	objectKey := conn.Path
+	if bucket == "" || bucket == "/" {
+		return errors.Join(errors.ErrUnsupported, fmt.Errorf("bucket must be specified"))
+	}
+	if objectKey == "" || objectKey == "/" {
+		return errors.Join(errors.ErrUnsupported, fmt.Errorf("object key must be specified"))
+	}
+	if recursive {
+		entries, err := c.client.ListObjectsV2(ctx, &s3.ListObjectsV2Input{
+			Bucket: aws.String(bucket),
+			Prefix: aws.String(objectKey),
+		})
+		if err != nil {
+			return err
+		}
+		if len(entries.Contents) == 0 {
+			return nil
+		}
+		objects := make([]types.ObjectIdentifier, 0, len(entries.Contents))
+		for _, obj := range entries.Contents {
+			objects = append(objects, types.ObjectIdentifier{Key: obj.Key})
+		}
+		_, err = c.client.DeleteObjects(ctx, &s3.DeleteObjectsInput{
+			Bucket: aws.String(bucket),
+			Delete: &types.Delete{
+				Objects: objects,
+			},
+		})
+		return err
+	}
+	_, err := c.client.DeleteObject(ctx, &s3.DeleteObjectInput{
+		Bucket: aws.String(bucket),
+		Key:    aws.String(objectKey),
+	})
+	return err
 }
 
 func (c S3Client) Join(ctx context.Context, conn *connparse.Connection, parts ...string) (string, error) {
-	return "", errors.ErrUnsupported
+	var joinParts []string
+	if conn.Host == "" || conn.Host == "/" {
+		if conn.Path == "" || conn.Path == "/" {
+			joinParts = parts
+		} else {
+			joinParts = append([]string{conn.Path}, parts...)
+		}
+	} else if conn.Path == "" || conn.Path == "/" {
+		joinParts = append([]string{conn.Host}, parts...)
+	} else {
+		joinParts = append([]string{conn.Host, conn.Path}, parts...)
+	}
+
+	return fmt.Sprintf("%s://%s", conn.Scheme, strings.Join(joinParts, "/")), nil
 }
 
 func (c S3Client) GetConnectionType() string {
 	return connparse.ConnectionTypeS3
+}
+
+func getParentPathUri(conn *connparse.Connection) string {
+	parentPath := getParentPath(conn)
+	if parentPath == "" {
+		return ""
+	}
+	return fmt.Sprintf("%s://%s", conn.Scheme, parentPath)
+}
+
+func getParentPath(conn *connparse.Connection) string {
+	var parentPath string
+	hostAndPath := conn.GetPathWithHost()
+	slashIndices := slashRe.FindAllStringIndex(hostAndPath, -1)
+	if slashIndices != nil && len(slashIndices) > 0 {
+		if slashIndices[len(slashIndices)-1][0] != len(hostAndPath)-1 {
+			parentPath = hostAndPath[:slashIndices[len(slashIndices)-1][0]+1]
+		} else if len(slashIndices) > 1 {
+			parentPath = hostAndPath[:slashIndices[len(slashIndices)-2][0]+1]
+		}
+	}
+	return parentPath
+
 }
