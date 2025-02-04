377b2e1ae7838df7090bbaffcef276ee60c5189c
sawka <mike@commandline.dev>
2025-02-03 22:23:20 -0800

    working on better tailwind setup

--- frontend/app/shadcn/chart.tsx
+++ frontend/app/shadcn/chart.tsx
@@ -156,7 +156,7 @@ const ChartTooltipContent = React.forwardRef<
             <div
                 ref={ref}
                 className={cn(
-                    "grid min-w-[8rem] items-start gap-1.5 rounded-lg border border-border/50 bg-background px-2.5 py-1.5 text-xs shadow-xl",
+                    "grid min-w-[8rem] items-start gap-1.5 rounded-lg border border-border/50 bg-background !px-2.5 !py-1.5 text-xs shadow-xl",
                     className
                 )}
             >
