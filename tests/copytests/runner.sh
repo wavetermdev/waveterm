b9a7bb029b856da33e1053b7bd14d1990b1d54c7
Sylvia Crowe <software@oneirocosm.com>
2025-02-07 12:00:31 -0800

    feat: add a tests passed counter

--- tests/copytests/runner.sh
+++ tests/copytests/runner.sh
@@ -3,6 +3,9 @@
 cd "$(dirname "$0")"
 source testutil.sh
 
+TOTAL_COPY_TESTS_RUN=0
+TOTAL_COPY_TESTS_PASSED=0
+
 for fname in cases/*.sh; do
     setup_testcp
     #"${fname}" | read outerr && printf "\e[32mPASS $fname\n\n\e[0m" || printf "\e[31mFAIL $fname: $outerr \n\n\e[0m"
@@ -12,6 +15,10 @@ for fname in cases/*.sh; do
 		printf "\n"
     else
         printf "\e[32mPASS $fname\n\n\e[0m"
+        ((TOTAL_COPY_TESTS_PASSED++))
     fi
     cleanup_testcp
+	((TOTAL_COPY_TESTS_RUN++))
 done
+
+printf "\n\e[32m${TOTAL_COPY_TESTS_PASSED} of ${TOTAL_COPY_TESTS_RUN} Tests Passed \e[0m\n\n"
