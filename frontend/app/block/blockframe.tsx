e91be98a140aae66625c7739ed36db1158c0b3c0
sawka <mike@commandline.dev>
2025-02-10 16:23:39 -0800

    added some docs to ViewModel and also fixed the 'inert' type error

--- frontend/app/block/blockframe.tsx
+++ frontend/app/block/blockframe.tsx
@@ -607,7 +607,7 @@ const BlockFrame_Default_Component = (props: BlockFrameProps) => {
                     "--magnified-block-blur": `${magnifiedBlockBlur}px`,
                 } as React.CSSProperties
             }
-            inert={preview ? "1" : undefined} // this does exist in the DOM, just not in react
+            {...({ inert: preview ? "1" : undefined } as any)} // sets insert="1" ... but tricks TS into accepting it
         >
             <BlockMask nodeModel={nodeModel} />
             {preview || viewModel == null ? null : (
