70c23a89a317984c6ae07624ec849778f6735ef2
Sylvia Crowe <software@oneirocosm.com>
2025-02-13 22:02:28 -0800

    test: fix behavior of test026
    This updates the test behavior to match its description

--- tests/copytests/cases/test026.sh
+++ tests/copytests/cases/test026.sh
@@ -7,9 +7,9 @@ cd "$HOME/testcp"
 touch foo.txt
 
 # this is different from cp behavior
-wsh file copy foo.txt baz/ >/dev/null 2>&1 && echo "command should have failed" && exit 1
+wsh file copy foo.txt baz/
 
-if [ -f baz/foo.txt ]; then
-    echo "baz/foo.txt should not exist"
+if [ ! -f baz/foo.txt ]; then
+    echo "baz/foo.txt does not exist"
     exit 1
 fi
