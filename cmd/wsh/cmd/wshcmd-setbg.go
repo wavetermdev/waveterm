9150a7f6b3d261d62a4b041c61fe98c63587c316
sawka <mike@commandline.dev>
2025-02-13 21:09:29 -0800

    fix clear in setbg

--- cmd/wsh/cmd/wshcmd-setbg.go
+++ cmd/wsh/cmd/wshcmd-setbg.go
@@ -90,7 +90,11 @@ func setBgRun(cmd *cobra.Command, args []string) (rtnErr error) {
 		if setBgOpacity < 0 || setBgOpacity > 1 {
 			return fmt.Errorf("opacity must be between 0.0 and 1.0")
 		}
-		meta["bg:opacity"] = setBgOpacity
+		if setBgClear {
+			meta["bg:*"] = true
+		} else {
+			meta["bg:opacity"] = setBgOpacity
+		}
 	} else if len(args) > 1 {
 		OutputHelpMessage(cmd)
 		return fmt.Errorf("too many arguments")
