390ae7a0270c59cee1cc4a7ed874a662ba819680
sawka <mike@commandline.dev>
2025-02-14 13:15:55 -0800

    set timeout val to null after clear

--- emain/emain-tabview.ts
+++ emain/emain-tabview.ts
@@ -105,6 +105,7 @@ export class WaveTabView extends WebContentsView {
         } else {
             if (this.resetChordModeTimeout) {
                 clearTimeout(this.resetChordModeTimeout);
+                this.resetChordModeTimeout = null;
             }
         }
     }
