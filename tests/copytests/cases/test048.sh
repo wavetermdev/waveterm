c979dced970d6c8fa80dc4210dc28ef747c4d277
Sylvia Crowe <software@oneirocosm.com>
2025-02-13 22:38:09 -0800

    test: split up test 48 and 49
    We previously only handle the case of copying the current directory to
    an existing directory. This adds another case for when the target
    directory does not exist.

--- tests/copytests/cases/test048.sh
+++ tests/copytests/cases/test048.sh
@@ -1,14 +1,17 @@
-# copy the current directory into a non-existing directory
+# copy the current directory into an existing directory
 # ensure the copy succeeds and the output exists
 
 set -e
 cd "$HOME/testcp"
 mkdir foo
 touch foo/bar.txt
+mkdir baz
 cd foo
+
 
 wsh file copy . ../baz
 cd ..
+
 
 if [ ! -f baz/bar.txt ]; then
     echo "baz/bar.txt does not exist"
