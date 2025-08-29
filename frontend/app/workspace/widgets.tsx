e79af2274ed00eb1c26a1f515e74b6a4ce603b13
sawka <mike@commandline.dev>
2025-08-29 11:45:16 -0700

    split workspace into workspace/widgets files

---
+++ frontend/app/workspace/widgets.tsx
@@ -0,0 +1,137 @@
+// Copyright 2025, Command Line Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+import { NotificationPopover } from "@/app/notification/notificationpopover";
+import { ContextMenuModel } from "@/app/store/contextmenu";
+import { RpcApi } from "@/app/store/wshclientapi";
+import { TabRpcClient } from "@/app/store/wshrpcutil";
+import { atoms, createBlock, getApi, isDev } from "@/store/global";
+import { fireAndForget, isBlank, makeIconClass } from "@/util/util";
+import clsx from "clsx";
+import { useAtomValue } from "jotai";
+import { memo } from "react";
+
+function sortByDisplayOrder(wmap: { [key: string]: WidgetConfigType }): WidgetConfigType[] {
+    if (wmap == null) {
+        return [];
+    }
+    const wlist = Object.values(wmap);
+    wlist.sort((a, b) => {
+        return (a["display:order"] ?? 0) - (b["display:order"] ?? 0);
+    });
+    return wlist;
+}
+
+async function handleWidgetSelect(widget: WidgetConfigType) {
+    const blockDef = widget.blockdef;
+    createBlock(blockDef, widget.magnified);
+}
+
+const Widget = memo(({ widget }: { widget: WidgetConfigType }) => {
+    return (
+        <div
+            className={clsx(
+                "flex flex-col justify-center items-center w-full py-1.5 pr-0.5 text-secondary text-lg overflow-hidden rounded-sm hover:bg-hoverbg hover:text-white cursor-pointer",
+                widget["display:hidden"] && "hidden"
+            )}
+            onClick={() => handleWidgetSelect(widget)}
+            title={widget.description || widget.label}
+        >
+            <div style={{ color: widget.color }}>
+                <i className={makeIconClass(widget.icon, true, { defaultIcon: "browser" })}></i>
+            </div>
+            {!isBlank(widget.label) ? (
+                <div className="text-xxs mt-0.5 w-full px-0.5 text-center whitespace-nowrap overflow-hidden">
+                    {widget.label}
+                </div>
+            ) : null}
+        </div>
+    );
+});
+
+const Widgets = memo(() => {
+    const fullConfig = useAtomValue(atoms.fullConfigAtom);
+    const helpWidget: WidgetConfigType = {
+        icon: "circle-question",
+        label: "help",
+        blockdef: {
+            meta: {
+                view: "help",
+            },
+        },
+    };
+    const tipsWidget: WidgetConfigType = {
+        icon: "lightbulb",
+        label: "tips",
+        blockdef: {
+            meta: {
+                view: "tips",
+            },
+        },
+    };
+    const showHelp = fullConfig?.settings?.["widget:showhelp"] ?? true;
+    const widgets = sortByDisplayOrder(fullConfig?.widgets);
+
+    const handleWidgetsBarContextMenu = (e: React.MouseEvent) => {
+        e.preventDefault();
+        const menu: ContextMenuItem[] = [
+            {
+                label: "Edit widgets.json",
+                click: () => {
+                    fireAndForget(async () => {
+                        const path = `${getApi().getConfigDir()}/widgets.json`;
+                        const blockDef: BlockDef = {
+                            meta: { view: "preview", file: path },
+                        };
+                        await createBlock(blockDef, false, true);
+                    });
+                },
+            },
+            {
+                label: "Show Help Widgets",
+                submenu: [
+                    {
+                        label: "On",
+                        type: "checkbox",
+                        checked: showHelp,
+                        click: () => {
+                            fireAndForget(async () => {
+                                await RpcApi.SetConfigCommand(TabRpcClient, { "widget:showhelp": true });
+                            });
+                        },
+                    },
+                    {
+                        label: "Off",
+                        type: "checkbox",
+                        checked: !showHelp,
+                        click: () => {
+                            fireAndForget(async () => {
+                                await RpcApi.SetConfigCommand(TabRpcClient, { "widget:showhelp": false });
+                            });
+                        },
+                    },
+                ],
+            },
+        ];
+        ContextMenuModel.showContextMenu(menu, e);
+    };
+
+    return (
+        <div
+            className="flex flex-col w-12 overflow-hidden py-1 -ml-1 select-none"
+            onContextMenu={handleWidgetsBarContextMenu}
+        >
+            {widgets?.map((data, idx) => <Widget key={`widget-${idx}`} widget={data} />)}
+            <div className="flex-grow" />
+            {showHelp ? (
+                <>
+                    <Widget key="tips" widget={tipsWidget} />
+                    <Widget key="help" widget={helpWidget} />
+                </>
+            ) : null}
+            {isDev() ? <NotificationPopover /> : null}
+        </div>
+    );
+});
+
+export { Widgets };
