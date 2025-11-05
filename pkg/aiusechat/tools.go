03f776a301e3cf62c461d399d86ddc143bd08374
Mike Sawka <mike@commandline.dev>
2025-11-04 16:39:19 -0800

    remove the redundant term_command_output tool (#2522)

--- pkg/aiusechat/tools.go
+++ pkg/aiusechat/tools.go
@@ -178,7 +178,7 @@ func GenerateTabStateAndTools(ctx context.Context, tabid string, widgetAccess bo
 		}
 		if viewTypes["term"] {
 			tools = append(tools, GetTermGetScrollbackToolDefinition(tabid))
-			tools = append(tools, GetTermCommandOutputToolDefinition(tabid))
+			// tools = append(tools, GetTermCommandOutputToolDefinition(tabid))
 		}
 		if viewTypes["web"] {
 			tools = append(tools, GetWebNavigateToolDefinition(tabid))
