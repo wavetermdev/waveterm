c3e995b3844258e13d9169065987b27dd67ee1eb
sawka <mike@commandline.dev>
2026-02-07 13:17:38 -0800

    input handling for connmonitor, sets urgency, lower threshold for stall indicator

--- pkg/remote/conncontroller/connmonitor.go
+++ pkg/remote/conncontroller/connmonitor.go
@@ -17,19 +17,22 @@ type ConnMonitor struct {
 	lock              *sync.Mutex
 	Conn              *SSHConn
 	LastActivityTime  atomic.Int64
+	LastInputTime     atomic.Int64
 	KeepAliveSentTime int64
 	KeepAliveInFlight bool
 	ctx               context.Context
 	cancelFunc        context.CancelFunc
+	inputNotifyCh     chan int64
 }
 
 func MakeConnMonitor(conn *SSHConn) *ConnMonitor {
 	ctx, cancelFunc := context.WithCancel(context.Background())
 	cm := &ConnMonitor{
-		lock:       &sync.Mutex{},
-		Conn:       conn,
-		ctx:        ctx,
-		cancelFunc: cancelFunc,
+		lock:          &sync.Mutex{},
+		Conn:          conn,
+		ctx:           ctx,
+		cancelFunc:    cancelFunc,
+		inputNotifyCh: make(chan int64, 1),
 	}
 	go cm.keepAliveMonitor()
 	return cm
@@ -38,6 +41,23 @@ func MakeConnMonitor(conn *SSHConn) *ConnMonitor {
 func (cm *ConnMonitor) UpdateLastActivityTime() {
 	cm.LastActivityTime.Store(time.Now().UnixMilli())
 	cm.Conn.SetConnHealthStatus(ConnHealthStatus_Good)
+}
+
+func (cm *ConnMonitor) NotifyInput() {
+	inputTime := time.Now().UnixMilli()
+	cm.LastInputTime.Store(inputTime)
+	select {
+	case cm.inputNotifyCh <- inputTime:
+	default:
+	}
+}
+
+func (cm *ConnMonitor) isUrgent() bool {
+	lastInput := cm.LastInputTime.Load()
+	if lastInput == 0 {
+		return false
+	}
+	return time.Now().UnixMilli()-lastInput < 10000
 }
 
 func (cm *ConnMonitor) setKeepAliveInFlight() bool {
@@ -88,6 +108,32 @@ func (cm *ConnMonitor) SendKeepAlive() error {
 	return nil
 }
 
+func (cm *ConnMonitor) checkConnection() {
+	lastActivity := cm.LastActivityTime.Load()
+	if lastActivity == 0 {
+		return
+	}
+	urgent := cm.isUrgent()
+	timeSinceActivity := time.Now().UnixMilli() - lastActivity
+
+	keepAliveThreshold := int64(10000)
+	if urgent {
+		keepAliveThreshold = 1000
+	}
+	if timeSinceActivity > keepAliveThreshold {
+		cm.SendKeepAlive()
+	}
+
+	stalledThreshold := int64(10000)
+	if urgent {
+		stalledThreshold = 5000
+	}
+	timeSinceKeepAlive := cm.getTimeSinceKeepAlive()
+	if timeSinceKeepAlive > stalledThreshold {
+		cm.Conn.SetConnHealthStatus(ConnHealthStatus_Stalled)
+	}
+}
+
 func (cm *ConnMonitor) keepAliveMonitor() {
 	defer func() {
 		panichandler.PanicHandler("conncontroller:keepAliveMonitor", recover())
@@ -98,18 +144,19 @@ func (cm *ConnMonitor) keepAliveMonitor() {
 	for {
 		select {
 		case <-ticker.C:
-			lastActivity := cm.LastActivityTime.Load()
-			if lastActivity == 0 {
-				continue
-			}
-			timeSinceActivity := time.Now().UnixMilli() - lastActivity
-			if timeSinceActivity > 10000 {
-				cm.SendKeepAlive()
-				timeSinceKeepAlive := cm.getTimeSinceKeepAlive()
-				if timeSinceKeepAlive > 10000 {
-					cm.Conn.SetConnHealthStatus(ConnHealthStatus_Stalled)
+			cm.checkConnection()
+
+		case inputTime := <-cm.inputNotifyCh:
+			select {
+			case <-time.After(1 * time.Second):
+				if cm.LastActivityTime.Load() >= inputTime {
+					break
 				}
+				cm.checkConnection()
+			case <-cm.ctx.Done():
+				return
 			}
+
 		case <-cm.ctx.Done():
 			return
 		}
