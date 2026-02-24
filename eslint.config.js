235c70e6f05f5c98cf02cfdec4af025f1f9252f7
sawka <mike@commandline.dev>
2026-02-23 16:38:28 -0800

    fix eslint flat config, add globals

--- eslint.config.js
+++ eslint.config.js
@@ -2,20 +2,21 @@
 
 import eslint from "@eslint/js";
 import eslintConfigPrettier from "eslint-config-prettier";
+import globals from "globals";
 import tseslint from "typescript-eslint";
 
-const baseConfig = tseslint.config(eslint.configs.recommended, ...tseslint.configs.recommended);
+export default [
+    eslint.configs.recommended,
+    ...tseslint.configs.recommended,
 
-const customConfig = {
-    ...baseConfig,
-    overrides: [
-        {
-            files: ["emain/emain.ts", "electron.vite.config.ts"],
-            env: {
-                node: true,
+    {
+        files: ["emain/emain.ts", "electron.vite.config.ts"],
+        languageOptions: {
+            globals: {
+                ...globals.node,
             },
         },
-    ],
-};
+    },
 
-export default [customConfig, eslintConfigPrettier];
+    eslintConfigPrettier,
+];
