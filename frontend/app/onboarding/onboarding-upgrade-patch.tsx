6dc91f34b53ceee435305bba48c56d58b36742fd
sawka <mike@commandline.dev>
2026-03-25 13:29:40 -0700

    add onboarding screen for v0.14.4

--- frontend/app/onboarding/onboarding-upgrade-patch.tsx
+++ frontend/app/onboarding/onboarding-upgrade-patch.tsx
@@ -25,6 +25,7 @@ import { UpgradeOnboardingModal_v0_13_1_Content } from "./onboarding-upgrade-v01
 import { UpgradeOnboardingModal_v0_14_0_Content } from "./onboarding-upgrade-v0140";
 import { UpgradeOnboardingModal_v0_14_1_Content } from "./onboarding-upgrade-v0141";
 import { UpgradeOnboardingModal_v0_14_2_Content } from "./onboarding-upgrade-v0142";
+import { UpgradeOnboardingModal_v0_14_4_Content } from "./onboarding-upgrade-v0144";
 
 interface VersionConfig {
     version: string;
@@ -139,6 +140,12 @@ export const UpgradeOnboardingVersions: VersionConfig[] = [
         version: "v0.14.3",
         content: () => <UpgradeOnboardingModal_v0_14_2_Content />,
         prevText: "Prev (v0.14.1)",
+        nextText: "Next (v0.14.4)",
+    },
+    {
+        version: "v0.14.4",
+        content: () => <UpgradeOnboardingModal_v0_14_4_Content />,
+        prevText: "Prev (v0.14.3)",
     },
 ];
 
